25be8bfa4789d367d131b381afb22e1ec347d7d4
Laurent Pugin <lxpugin@gmail.com>
2014-02-05 20:45:22 +0100

    removing convertMusic function

--- emscripten/emscripten_main.cpp
+++ emscripten/emscripten_main.cpp
@@ -26,35 +26,7 @@ char *_persistent_log_string = NULL;
 
 
 extern "C" {
-    const char * convertMusic(const char * c_notation, const char * c_options) {
-        
-        string notation(c_notation);
-        string json_options(c_options);
-        string out_str;
-        InterfaceController controller;
-        
-        if (!initialized) {
-            std::srand(std::time(0));
-            initialized = true;
-        }
-        
-        if (!controller.ParseOptions( json_options )) {
-            vrv::LogError( "Could not load JSON options." );
-            return NULL;
-        }
-        
-        Resources::SetPath("/data");
-        
-        // default to mei if unset.
-        controller.LoadString( notation );
 
-        // in the future we will be able to render to mei too
-        out_str = controller.RenderToSvg();
-        return out_str.c_str();
-        // return self allocated pointer?
-        // see https://github.com/DDMAL/aruspix/commit/cb2b957ebcc8edf9787bc6f6271217115a6959e3
-    }
-	
 	/****************************************************************
 	* Methods exported to use the InterfaceController class from js
 	****************************************************************/
