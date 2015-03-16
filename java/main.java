8686ffcfb63fd7276c5236b55a3c41ca9b976511
Rodolfo Zitellini <rwz@xhero.org>
2015-03-16 14:14:51 +0100

    Add java target using maven

---
+++ java/main.java
@@ -0,0 +1,12 @@
+public class main {
+   public static void main(String argv[]) {
+     System.loadLibrary("verovio");
+	
+     org.rism.verovio.toolkit toolkit = new org.rism.verovio.toolkit();
+	 
+	 toolkit.loadFile("PINO");
+
+   }
+ }
+
+
