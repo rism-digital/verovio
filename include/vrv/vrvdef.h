0e1f2ffc82baad3d5d80dac452a00c6fddf0fee2
Laurent Pugin <lxpugin@gmail.com>
2015-09-18 09:58:06 +0200

    Starting 0.9.10-dev

--- include/vrv/vrvdef.h
+++ include/vrv/vrvdef.h
@@ -27,9 +27,9 @@ namespace vrv {
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 9
-#define VERSION_REVISION 9
+#define VERSION_REVISION 10
 // Adds "-dev" in the version number - should be set to false for releases
-#define VERSION_DEV false
+#define VERSION_DEV true
 
 //----------------------------------------------------------------------------
 // Typedefs
