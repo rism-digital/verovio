951a1e90cd73a18fc7b3ea1f6e535a2d6888ade1
Laurent Pugin <lxpugin@gmail.com>
2016-09-13 20:42:00 +0200

    Building release

--- include/vrv/vrvdef.h
+++ include/vrv/vrvdef.h
@@ -27,7 +27,7 @@ namespace vrv {
 #define VERSION_MINOR 9
 #define VERSION_REVISION 12
 // Adds "-dev" in the version number - should be set to false for releases
-#define VERSION_DEV true
+#define VERSION_DEV false
 
 //----------------------------------------------------------------------------
 // Object defines
