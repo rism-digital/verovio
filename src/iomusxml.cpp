119565df222b29ce67bd057e55d819885aee30da
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2017-01-13 10:07:09 +0100

    adds support for non-direction tempo in MusicXML import

--- src/iomusxml.cpp
+++ src/iomusxml.cpp
@@ -388,6 +388,9 @@ bool MusicXmlInput::ReadMusicXml(pugi::xml_node root)
 
     int staffOffset = 0;
 
+    pugi::xpath_node scoreMidiBpm = root.select_single_node("/score-partwise/part[1]/measure[1]/sound[@tempo][1]");
+    if (scoreMidiBpm) m_doc->m_scoreDef.SetMidiBpm(atoi(GetAttributeValue(scoreMidiBpm.node(), "tempo").c_str()));
+    
     pugi::xpath_node_set partListChildren = root.select_nodes("/score-partwise/part-list/*");
     for (pugi::xpath_node_set::const_iterator it = partListChildren.begin(); it != partListChildren.end(); ++it) {
         pugi::xpath_node xpathNode = *it;
