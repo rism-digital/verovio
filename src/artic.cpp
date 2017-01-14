7daa2adae88bdfbb1bae94cd2ba055a019dbce1d
Laurent Pugin <lxpugin@gmail.com>
2017-01-14 17:18:54 +0000

    Adding support for harm

--- src/artic.cpp
+++ src/artic.cpp
@@ -24,9 +24,9 @@
 namespace vrv {
 
 std::vector<data_ARTICULATION> Artic::s_outStaffArtic
-    = { ARTICULATION_acc, ARTICULATION_dnbow, ARTICULATION_marc, ARTICULATION_marc_stacc, ARTICULATION_upbow };
+    = { ARTICULATION_acc, ARTICULATION_dnbow, ARTICULATION_marc, ARTICULATION_marc_stacc, ARTICULATION_upbow, ARTICULATION_harm };
 
-std::vector<data_ARTICULATION> Artic::s_aboveStaffArtic = { ARTICULATION_dnbow, ARTICULATION_marc, ARTICULATION_upbow };
+std::vector<data_ARTICULATION> Artic::s_aboveStaffArtic = { ARTICULATION_dnbow, ARTICULATION_marc, ARTICULATION_upbow, ARTICULATION_harm };
 
 //----------------------------------------------------------------------------
 // Artic
@@ -151,7 +151,7 @@ wchar_t Artic::GetSmuflCode(data_ARTICULATION artic, data_STAFFREL place)
             case ARTICULATION_dnbow: return SMUFL_E610_stringsDownBow;
             case ARTICULATION_upbow:
                 return SMUFL_E612_stringsUpBow;
-            // case ARTICULATION_harm;
+            case ARTICULATION_harm: return SMUFL_E614_stringsHarmonic;
             // case ARTICULATION_snap;
             // case ARTICULATION_fingernail;
             case ARTICULATION_ten_stacc:
@@ -185,6 +185,7 @@ wchar_t Artic::GetSmuflCode(data_ARTICULATION artic, data_STAFFREL place)
             case ARTICULATION_dnbow: return SMUFL_E611_stringsDownBowTurned;
             case ARTICULATION_upbow:
                 return SMUFL_E613_stringsUpBowTurned;
+            case ARTICULATION_harm: return SMUFL_E614_stringsHarmonic;
             //
             case ARTICULATION_ten_stacc:
                 return SMUFL_E4B3_articTenutoStaccatoBelow;
