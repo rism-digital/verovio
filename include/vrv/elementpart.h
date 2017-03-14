13b9c92f62249f601094f7969df96eae224f0b5b
Laurent Pugin <lxpugin@gmail.com>
2017-03-14 07:40:51 +0100

    Adding Flag and NoteHead classes

---
+++ include/vrv/elementpart.h
@@ -0,0 +1,138 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        elementpart.h
+// Author:      Laurent Pugin
+// Created:     2017
+// Copyright (c) Authors and others. All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __VRV_ELEMENT_PART_H__
+#define __VRV_ELEMENT_PART_H__
+
+#include "layerelement.h"
+
+namespace vrv {
+
+//----------------------------------------------------------------------------
+// Flag
+//----------------------------------------------------------------------------
+
+/**
+ * This class models a stem as a layer element part and has not direct MEI equivlatent.
+ */
+class Flag : public LayerElement {
+public:
+    /**
+     * @name Constructors, destructors, reset and class name methods
+     * Reset method resets all attribute classes
+     */
+    ///@{
+    Flag();
+    virtual ~Flag();
+    virtual void Reset();
+    virtual std::string GetClassName() const { return "Flag"; }
+    virtual ClassId GetClassId() const { return FLAG; }
+    ///@}
+    
+    //----------//
+    // Functors //
+    //----------//
+    
+    /**
+     * Overwritten version of Save that avoids anything to be written
+     */
+    ///@{
+    virtual int Save(FunctorParams *functorParams) { return FUNCTOR_CONTINUE; }
+    virtual int SaveEnd(FunctorParams *functorParams) { return FUNCTOR_CONTINUE; }
+    ///@}
+    
+private:
+    //
+public:
+    //
+private:
+};
+
+//----------------------------------------------------------------------------
+// NoteHead
+//----------------------------------------------------------------------------
+
+/**
+ * This class models a stem as a layer element part and has not direct MEI equivlatent.
+ */
+class NoteHead : public LayerElement {
+public:
+    /**
+     * @name Constructors, destructors, reset and class name methods
+     * Reset method resets all attribute classes
+     */
+    ///@{
+    NoteHead();
+    virtual ~NoteHead();
+    virtual void Reset();
+    virtual std::string GetClassName() const { return "NoteHead"; }
+    virtual ClassId GetClassId() const { return NOTEHEAD; }
+    ///@}
+    
+    //----------//
+    // Functors //
+    //----------//
+    
+    /**
+     * Overwritten version of Save that avoids anything to be written
+     */
+    ///@{
+    virtual int Save(FunctorParams *functorParams) { return FUNCTOR_CONTINUE; }
+    virtual int SaveEnd(FunctorParams *functorParams) { return FUNCTOR_CONTINUE; }
+    ///@}
+
+private:
+    //
+public:
+    //
+private:
+};
+    
+    
+//----------------------------------------------------------------------------
+// Stem
+//----------------------------------------------------------------------------
+
+/**
+ * This class models a stem as a layer element part and has not direct MEI equivlatent.
+ */
+class Stem : public LayerElement {
+public:
+    /**
+     * @name Constructors, destructors, reset and class name methods
+     * Reset method resets all attribute classes
+     */
+    ///@{
+    Stem();
+    virtual ~Stem();
+    virtual void Reset();
+    virtual std::string GetClassName() const { return "Stem"; }
+    virtual ClassId GetClassId() const { return STEM; }
+    ///@}
+    
+    //----------//
+    // Functors //
+    //----------//
+    
+    /**
+     * Overwritten version of Save that avoids anything to be written
+     */
+    ///@{
+    virtual int Save(FunctorParams *functorParams) { return FUNCTOR_CONTINUE; }
+    virtual int SaveEnd(FunctorParams *functorParams) { return FUNCTOR_CONTINUE; }
+    ///@}
+    
+private:
+    //
+public:
+    //
+private:
+};
+
+} // namespace vrv
+
+#endif
---
+++ src/elementpart.cpp
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        elementpart.cpp
+// Author:      Laurent Pugin
+// Created:     2017
+// Copyright (c) Authors and others. All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+
+#include "elementpart.h"
+
+namespace vrv {
+
+//----------------------------------------------------------------------------
+// Flag
+//----------------------------------------------------------------------------
+
+Flag::Flag() : LayerElement("flag-")
+{
+    
+    Reset();
+}
+
+Flag::~Flag()
+{
+}
+
+void Flag::Reset()
+{
+    LayerElement::Reset();
+}
+
+//----------------------------------------------------------------------------
+// NoteHead
+//----------------------------------------------------------------------------
+
+NoteHead::NoteHead() : LayerElement("notehead-")
+{
+    
+    Reset();
+}
+
+NoteHead::~NoteHead()
+{
+}
+
+void NoteHead::Reset()
+{
+    LayerElement::Reset();
+}
+    
+//----------------------------------------------------------------------------
+// Stem
+//----------------------------------------------------------------------------
+
+Stem::Stem() : LayerElement("stem-")
+{
+ 
+    Reset();
+}
+
+Stem::~Stem()
+{
+}
+
+void Stem::Reset()
+{
+    LayerElement::Reset();
+}
+
+} // namespace vrv
