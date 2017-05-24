4117acc98e5bdb17b6c1e2541f760df5b8841e19
rettinghaus <klaus.rettinghaus@gmail.com>
2017-05-24 16:51:37 +0200

    better positioning of rests

--- src/rest.cpp
+++ src/rest.cpp
@@ -96,10 +96,23 @@ wchar_t Rest::GetRestGlyph() const
     return symc;
 }
 
-int Rest::GetDefaultLoc(bool hasMultipleLayer, bool isFirstLayer)
+int Rest::GetRestDefaultLoc(bool hasMultipleLayer, bool isFirstLayer)
 {
+    // only works if staff has 5 lines
     int loc = 4;
 
+    if (hasMultipleLayer) {
+        if (isFirstLayer)
+            loc += 2;
+        else
+            loc -= 2;
+    }
+
+    return loc;
+}
+
+int Rest::GetRestLocOffset(int loc)
+{
     switch (this->GetActualDur()) {
         case DUR_MX: loc -= 0; break;
         case DUR_LG: loc -= 0; break;
@@ -114,12 +127,6 @@ int Rest::GetDefaultLoc(bool hasMultipleLayer, bool isFirstLayer)
         case DUR_128: loc -= 2; break;
         case DUR_256: loc -= 2; break;
         default: loc -= 1; break;
-    }
-    if (hasMultipleLayer) {
-        if (isFirstLayer)
-            loc += 2;
-        else
-            loc -= 2;
     }
 
     return loc;
