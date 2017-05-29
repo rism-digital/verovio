76d393e34d9e60cda8e584e3e7e805250d47b2c9
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2017-05-29 20:44:10 +0200

    refining multiRest
    * centering multiRests in staff
    * adding initial support for `@block`

--- src/iomei.cpp
+++ src/iomei.cpp
@@ -1158,6 +1158,7 @@ void MeiOutput::WriteMeiMultiRest(pugi::xml_node currentNode, MultiRest *multiRe
     assert(multiRest);
 
     WriteLayerElement(currentNode, multiRest);
+    multiRest->WriteMultiRestVis(currentNode);
     multiRest->WriteNumbered(currentNode);
 }
 
@@ -3097,6 +3098,7 @@ bool MeiInput::ReadMeiMultiRest(Object *parent, pugi::xml_node multiRest)
     MultiRest *vrvMultiRest = new MultiRest();
     ReadLayerElement(multiRest, vrvMultiRest);
 
+    vrvMultiRest->ReadMultiRestVis(multiRest);
     vrvMultiRest->ReadNumbered(multiRest);
 
     parent->AddChild(vrvMultiRest);
