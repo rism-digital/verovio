a55f81f09a49596908c0ab746d3e8d912b103413
Laurent Pugin <lxpugin@gmail.com>
2017-06-20 12:34:09 +0300

    Fixing grace slash missing

--- src/note.cpp
+++ src/note.cpp
@@ -604,6 +604,7 @@ int Note::PrepareLayerElementParts(FunctorParams *functorParams)
             currentStem = new Stem();
             this->AddChild(currentStem);
         }
+        currentStem->AttGraced::operator=(*this);
         currentStem->AttStems::operator=(*this);
         currentStem->AttStemsCmn::operator=(*this);
     }
