62b6389251b8b4cb70cbcfd8fca3433f100dc528
Laurent Pugin <lxpugin@gmail.com>
2017-07-11 10:21:33 +0200

    Improving PAE mensural

--- include/vrv/iopae.h
+++ include/vrv/iopae.h
@@ -24,6 +24,7 @@ class Layer;
 class LayerElement;
 class Measure;
 class MeterSig;
+class Mensur;
 class Note;
 class Staff;
 class Tie;
@@ -152,6 +153,7 @@ namespace pae {
         { // for STL vector
             clef = d.clef;
             meter = d.meter;
+            mensur = d.mensur;
             notes = d.notes;
 
             key = d.key;
@@ -169,6 +171,7 @@ namespace pae {
         { // for STL vector
             clef = d.clef;
             meter = d.meter;
+            mensur = d.mensur;
             notes = d.notes;
 
             key = d.key;
@@ -194,6 +197,7 @@ namespace pae {
         {
             clef = NULL;
             meter = NULL;
+            mensur = NULL;
             key = NULL;
             notes.clear();
             barLine = BARRENDITION_invis;
@@ -201,6 +205,7 @@ namespace pae {
             abbreviation_offset = -1;
         }
         Clef *clef;
+        Mensur *mensur;
         MeterSig *meter;
         KeySig *key;
 
@@ -238,7 +243,7 @@ private:
 
     // parsing functions
     int getKeyInfo(const char *incipit, KeySig *key, int index = 0);
-    int getTimeInfo(const char *incipit, MeterSig *meter, int index = 0);
+    int getTimeInfo(const char *incipit, MeterSig *meter, Mensur *mensur, int index = 0);
     int getClefInfo(const char *incipit, Clef *mus_clef, int index = 0);
     int getBarLine(const char *incipit, data_BARRENDITION *output, int index);
     int getAccidental(const char *incipit, data_ACCIDENTAL_WRITTEN *accident, int index = 0);
@@ -275,6 +280,7 @@ private:
     Layer *m_layer;
     Note *m_last_tied_note;
     bool m_is_in_chord;
+    bool m_is_mensural;
 
     std::vector<LayerElement *> m_nested_objects;
 };
