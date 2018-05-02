43c3d2effe30b60443cd54fb9657cbb837b01087
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2018-05-02 14:43:35 +0200

    updating midi numbers to latest MEI

--- src/iomusxml.cpp
+++ src/iomusxml.cpp
@@ -481,7 +481,7 @@ bool MusicXmlInput::ReadMusicXml(pugi::xml_node root)
             pugi::xpath_node midiInstrument = xpathNode.node().select_single_node("midi-instrument");
             pugi::xpath_node midiChannel = midiInstrument.node().select_single_node("midi-channel");
             pugi::xpath_node midiName = midiInstrument.node().select_single_node("midi-name");
-            pugi::xpath_node midiPan = midiInstrument.node().select_single_node("pan");
+            // pugi::xpath_node midiPan = midiInstrument.node().select_single_node("pan");
             pugi::xpath_node midiProgram = midiInstrument.node().select_single_node("midi-program");
             pugi::xpath_node midiVolume = midiInstrument.node().select_single_node("volume");
             // create the staffDef(s)
@@ -506,9 +506,9 @@ bool MusicXmlInput::ReadMusicXml(pugi::xml_node root)
                 if (midiInstrument) {
                     InstrDef *instrdef = new InstrDef;
                     instrdef->SetMidiInstrname(instrdef->AttMidiInstrument::StrToMidinames(midiName.node().text().as_string()));
-                    if (midiChannel) instrdef->SetMidiChannel(midiChannel.node().text().as_int());
-                    if (midiPan) instrdef->SetMidiPan(midiPan.node().text().as_int());
-                    if (midiProgram) instrdef->SetMidiInstrnum(midiProgram.node().text().as_int());
+                    if (midiChannel) instrdef->SetMidiChannel(midiChannel.node().text().as_int() - 1);
+                    // if (midiPan) instrdef->SetMidiPan(midiPan.node().text().as_int());
+                    if (midiProgram) instrdef->SetMidiInstrnum(midiProgram.node().text().as_int() - 1);
                     if (midiVolume) instrdef->SetMidiVolume(midiVolume.node().text().as_int());
                     partStaffGrp->AddChild(instrdef);
                 }
@@ -535,10 +535,10 @@ bool MusicXmlInput::ReadMusicXml(pugi::xml_node root)
                     }
                     if (midiInstrument) {
                         InstrDef *instrdef = new InstrDef;
-                        if (midiChannel) instrdef->SetMidiChannel(midiChannel.node().text().as_int());
-                        if (midiProgram) instrdef->SetMidiInstrnum(midiProgram.node().text().as_int());
+                        if (midiChannel) instrdef->SetMidiChannel(midiChannel.node().text().as_int() - 1);
+                        // if (midiPan) instrdef->SetMidiPan(midiPan.node().text().as_int());
+                        if (midiProgram) instrdef->SetMidiInstrnum(midiProgram.node().text().as_int() - 1);
                         if (midiVolume) instrdef->SetMidiVolume(midiVolume.node().text().as_int());
-                        if (midiPan) instrdef->SetMidiPan(midiPan.node().text().as_int());
                         staffDef->AddChild(instrdef);
                     }
                 }
