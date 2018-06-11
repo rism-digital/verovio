37d60414b5744517f3549d4f6f99fd42415c5d8c
Juliette Regimbal <juliette.regimbal@gmail.com>
2018-06-11 10:44:42 -0400

    Initial commit: facsimile

---
+++ include/vrv/facsimile.h
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        facsimile.h
+// Author:      Juliette Regimbal
+// Created:     2018
+// Copyright (c) Authors and others. All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __VRV_FACSIMILE_H__
+#define __VRV_FACSIMILE_H__
+
+#include <assert.h>
+
+//----------------------------------------------------------------------------
+
+#include "atts_analytical.h"
+#include "atts_shared.h"
+#include "vrvdef.h"
+
+namespace vrv {
+
+//----------------------------------------------------------------------------
+// Facsimile
+//----------------------------------------------------------------------------
+/**
+ * Implements the facsimile element
+ * in MEI
+ */
+
+class Facsimile : public Object {
+public:
+    /**
+     * @name Constructors, destructors, reset and class name methods
+     * Reset method resets all attribute classes
+     */
+    ///@{
+    Facsimile(); 
+    virtual ~Facsimile();
+    virtual void Reset();
+    ClassId GetClassId const { return FACSIMILE; }
+    ///@}
+};
+
+//----------------------------------------------------------------------------
+// Surface 
+//----------------------------------------------------------------------------
+/**
+ * Implements the surface element
+ * in MEI
+ */
+
+class Surface : public Object, public AttTyped, public AttCoordinated {
+public:
+    /**
+     * @name Constructors, destructors, reset, and class name methods
+     */
+    ///@{
+    Surface();
+    virtual ~Surface();
+    virtual void Reset();
+    ClassId GetClassId const { return SURFACE; }
+    ///@}
+private:
+};
+
+//----------------------------------------------------------------------------
+// Zone
+//----------------------------------------------------------------------------
+/**
+ * Implements the zone element
+ * in MEI
+ */
+
+class Zone : public Object, public AttTyped, public AttCoordinated {
+public:
+    /**
+     * @name Constructors, destructors, reset, and class name methods
+     */
+    ///@{
+    Zone();
+    virtual ~Zone();
+    virtual void Reset();
+    ClassId GetClassId const { return ZONE; }
+    ///@}
+private:
+};
+}
+#endif
---
+++ src/facsimile.cpp
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        facsimile.cpp
+// Author:      Juliette Regimbal
+// Created:     2018
+// Copyright (c) Authors and others. All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+
+#include "facsimile.h"
+
+//----------------------------------------------------------------------------
+
+#include <assert.h>
+
+//----------------------------------------------------------------------------
+
+#include "attcomparison.h"
+#include "object.h"
+#include "vrv.h"
+
+namespace vrv {
+
+//----------------------------------------------------------------------------
+// Facsimile
+//----------------------------------------------------------------------------
+Facsimile::Facsimile() : Object("facsimile-") {}
+Facsimile::~Facsimile() {}
+void Reset() {}
+//----------------------------------------------------------------------------
+// Surface 
+//----------------------------------------------------------------------------
+Surface::Surface() : Object("surface-"), AttTyped(), AttCoordinated()
+{
+    RegisterAttClass(ATT_TYPED);
+    RegisterAttClass(ATT_COORDINATED);
+    Reset();
+}
+Surface::~Surface() {}
+void Surface::Reset()
+{
+    ResetTyped();
+    ResetCoordinated();
+}
+
+//----------------------------------------------------------------------------
+// Zone 
+//----------------------------------------------------------------------------
+Zone::Zone() : Object(), AttTyped(), AttCoordinated() 
+{
+    RegisterAttClass(ATT_TYPED);
+    RegisterAttClass(ATT_COORDINATED);
+    Reset();
+}
+Zone::~Zone() {}
+void Zone::Reset()
+{
+    ResetTyped();
+    ResetCoordinated();
+}
+}
