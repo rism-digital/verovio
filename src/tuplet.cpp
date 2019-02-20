7487610c0309b29754f5323731815a3cfb0f7a9e
Laurent Pugin <lxpugin@gmail.com>
2019-02-20 14:05:44 +0100

    Adjusting num position for tuplets without bracket

--- src/tuplet.cpp
+++ src/tuplet.cpp
@@ -464,7 +464,7 @@ int Tuplet::AdjustTupletsY(FunctorParams *functorParams)
             Beam *beam = this->GetNumAlignedBeam();
             // If we have a beam first move it to the appropriate postion
             if (beam) {
-                int xMid = tupletNum->GetDrawingXMid();
+                int xMid = tupletNum->GetDrawingXMid(params->m_doc);
                 int yMid = beam->m_drawingParams.m_startingY
                     + beam->m_drawingParams.m_beamSlope * (xMid - beam->m_drawingParams.m_startingX);
                 int yMidRel = yMid - yReference;
