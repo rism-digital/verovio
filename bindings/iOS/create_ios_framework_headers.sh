a696f4115d00c575d8e19e3eb2bea382f84719a6
thomas.ramson <thomas.ramson@enote.com>
2019-06-14 11:02:28 +0200

    adjusting create header script to exclude win32 folder (can't be included on a mac system)

--- bindings/iOS/create_ios_framework_headers.sh
+++ bindings/iOS/create_ios_framework_headers.sh
@@ -8,7 +8,9 @@ echo "#ifndef all_h" > $output
 echo "#define all_h" >> $output
 
 for header in $(find ./include -name '*.h'); do
-echo "#import <VerovioFramework/$(basename $header)>" >> $output	
+    if [[ $header != *"win32"* ]]; then
+        echo "#import <VerovioFramework/$(basename $header)>" >> $output
+    fi
 done
 
 echo "#endif /* all_h */" >> $output
