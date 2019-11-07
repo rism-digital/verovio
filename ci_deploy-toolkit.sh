d6d8a7d4e6299658522c43f6c6a821e04b44024c
Stefan@Lap <stefan.muennich@unibas.ch>
2019-11-07 12:20:52 +0100

    test pushing to gh-pages

--- ci_deploy-toolkit.sh
+++ ci_deploy-toolkit.sh
@@ -12,31 +12,30 @@ if [ "${TRAVIS_BRANCH}" != "${BUILD_BRANCH}" ]; then
     exit 1
 fi
 
-# Get the rism-ch revision
-SHA=$(git rev-parse --verify HEAD)
-
 cd ${GH_PAGES_DIRECTORY}
 
 echo "Configuring git push"
 git config user.name "JavaScript toolkit builder"
 git config user.email "${COMMIT_AUTHOR_EMAIL}"
 
+echo "Running git remote show origin"
+git remote show origin
+
+echo "Running git add"
 git add -A
 
 echo "Running git status"
 git status
 
+echo "Get the rism-ch revision"
+SHA=$(git rev-parse --verify HEAD)
+
 echo "Running git commit"
 git commit -m "Auto-commit of toolkit build for rism-ch/verovio@${SHA}"
 
-git remote show origin
-# git remote show https://${GH_TOKEN}@github.com/musicEnfanthen/verovio   # TODO: remove this in production
-
 echo "Syncing from origin..."
-# git pull --verbose  # TODO: use this in production
-git pull https://${GH_TOKEN}@github.com/musicEnfanthen/verovio --verbose
+git pull --verbose
 
 echo "Pushing commits"
 # Now that we're all set up, we can push.
-# git push ${VEROVIO_REPOSITORY} ${GH_PAGES_DIRECTORY}  # TODO: use this in production
-git push https://${GH_TOKEN}@github.com/musicEnfanthen/verovio ${GH_PAGES_DIRECTORY}
+git push ${VEROVIO_REPOSITORY} ${GH_PAGES_DIRECTORY}
