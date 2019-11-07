1a6f0bf5caad9db317565f66e3d33c03cf8b9fb9
Stefan@Lap <stefan.muennich@unibas.ch>
2019-11-08 00:52:14 +0100

    test

--- ci_precache.sh
+++ ci_precache.sh
@@ -62,3 +62,31 @@ echo "Update the documentation of the option list"
 
 # Return to the root
 cd ..
+
+##################################
+# Push changes to temporary output
+##################################
+
+cd ${OUTPUT_DIRECTORY}
+git checkout ${TEMPORARY_OUTPUT_BRANCH}
+
+echo "Configuring git push"
+git config user.name "JavaScript toolkit builder"
+git config user.email "${COMMIT_AUTHOR_EMAIL}"
+
+echo "Running git add"
+git add -A
+
+echo "Running git commit"
+git commit -m "Auto-commit of precache toolkit build for rism-ch/verovio@${SHA}"
+
+echo "Syncing from origin..."
+# clean-up
+git fetch origin --prune
+# make sure that we are on the correct branch (temporary output)
+git checkout ${TEMPORARY_OUTPUT_BRANCH}
+git pull origin ${TEMPORARY_OUTPUT_BRANCH} --verbose
+
+echo "Pushing commits"
+# Now that we're all set up, we can push latest changes to temporary output
+git push origin ${TEMPORARY_OUTPUT_BRANCH}
