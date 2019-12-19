802ff475350e84e7d3162e00dcc33720f1b4b095
Laurent Pugin <lxpugin@gmail.com>
2019-12-19 16:45:47 +0000

    Adding PitchInflection class

---
+++ include/vrv/pitchinflection.h
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        pitchinfection.h
+// Author:      Laurent Pugin
+// Created:     2019
+// Copyright (c) Authors and others. All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __VRV_PITCH_INFLECTION_H__
+#define __VRV_PITCH_INFLECTION_H__
+
+#include "controlelement.h"
+#include "timeinterface.h"
+
+namespace vrv {
+
+//----------------------------------------------------------------------------
+// PitchInflection
+//----------------------------------------------------------------------------
+
+/**
+ * This class models the MEI <dir> element.
+ */
+class PitchInflection : public ControlElement, public TimeSpanningInterface {
+public:
+    /**
+     * @name Constructors, destructors, and other standard methods
+     * Reset method reset all attribute classes
+     */
+    ///@{
+    PitchInflection();
+    virtual ~PitchInflection();
+    virtual Object *Clone() const { return new PitchInflection(*this); }
+    virtual void Reset();
+    virtual std::string GetClassName() const { return "PitchInflection"; }
+    virtual ClassId GetClassId() const { return PITCHINFLECTION; }
+    ///@}
+
+    /**
+     * @name Getter to interfaces
+     */
+    ///@{
+    virtual TimePointInterface *GetTimePointInterface() { return dynamic_cast<TimePointInterface *>(this); }
+    virtual TimeSpanningInterface *GetTimeSpanningInterface() { return dynamic_cast<TimeSpanningInterface *>(this); }
+    ///@}
+
+    //----------//
+    // Functors //
+    //----------//
+
+protected:
+    //
+private:
+    //
+public:
+    //
+private:
+    //
+};
+
+} // namespace vrv
+
+#endif
