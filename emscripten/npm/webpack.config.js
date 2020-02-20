c9c2d290ba5572e69def9f03004adf33df6075d6
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2020-02-20 10:23:46 +0100

    Add webpack.config.js

---
+++ emscripten/npm/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd', // commonjs2, umd,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+};
