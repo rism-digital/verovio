4c2c90f452767504c6c070380e92038536a6595e
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2020-04-03 18:35:02 +0200

    add support for trill att.extender

--- src/trill.cpp
+++ src/trill.cpp
@@ -21,10 +21,12 @@ namespace vrv {
 // Trill
 //----------------------------------------------------------------------------
 
-Trill::Trill() : ControlElement("trill-"), TimeSpanningInterface(), AttColor(), AttOrnamentAccid(), AttPlacement()
+Trill::Trill()
+    : ControlElement("trill-"), TimeSpanningInterface(), AttColor(), AttExtender(), AttOrnamentAccid(), AttPlacement()
 {
     RegisterInterface(TimeSpanningInterface::GetAttClasses(), TimeSpanningInterface::IsInterface());
     RegisterAttClass(ATT_COLOR);
+    RegisterAttClass(ATT_EXTENDER);
     RegisterAttClass(ATT_ORNAMENTACCID);
     RegisterAttClass(ATT_PLACEMENT);
 
@@ -38,6 +40,7 @@ void Trill::Reset()
     ControlElement::Reset();
     TimeSpanningInterface::Reset();
     ResetColor();
+    ResetExtender();
     ResetOrnamentAccid();
     ResetPlacement();
 }
