rism-digital/verovio

commit 7beaab7a83dd4977c71d19bbb3ddf6a65dced681

Create Syllable::MarkupAddSyl

src/syllable.cpp
@@ -10,6 +10,7 @@
 
 //----------------------------------------------------------------------------
 
+#include <algorithm>
 #include <assert.h>
 #include <iostream>
 
@@ -17,6 +18,7 @@
 
 #include "neume.h"
 #include "syl.h"
+#include "text.h"
 #include "vrv.h"
 
 namespace vrv {
@@ -59,6 +61,29 @@ void Syllable::Reset()
     LayerElement::Reset();
     ResetColor();
     ResetSlashCount();
+}
+
+bool Syllable::MarkupAddSyl()
+{
+    // Check for a syl
+    Object *obj = this->FindDescendantByType(SYL);
+    if (obj != NULL) {  // A syl exists
+        return false;
+    }
+    // Check for follows attribute (extension of a syllable)
+    ArrayOfStrAttr attributes;
+    this->GetAttributes(&attributes);
+    bool hasFollows = std::find_if(attributes.begin(), attributes.end(), [](auto att) -> bool {
+        return std::string{ "follows" }.compare(att.first) == 0;
+    }) != attributes.end();
+    if (hasFollows) {
+        return false;
+    }
+    Syl *syl = new Syl();
+    Text *text = new Text();
+    syl->AddChild(text);
+    this->AddChild(text);
+    return true;
 }
 
 } // namespace vrv

include/vrv/syllable.h
@@ -47,6 +47,12 @@ public:
     /** Override the method since alignment is required */
     virtual bool HasToBeAligned() const { return true; }
 
+    /**
+     * Add a default syl to this syllable if one does not exist.
+     * Returns true if a syl was added, false otherwise.
+     */
+    bool MarkupAddSyl();
+
 private:
     //
 public:

src/doc.cpp
@@ -9,7 +9,6 @@
 
 //----------------------------------------------------------------------------
 
-#include <algorithm>
 #include <assert.h>
 #include <math.h>
 
@@ -52,6 +51,7 @@
 #include "staffdef.h"
 #include "staffgrp.h"
 #include "syl.h"
+#include "syllable.h"
 #include "system.h"
 #include "text.h"
 #include "timestamp.h"
@@ -734,18 +734,8 @@ void Doc::PrepareDrawing()
     ClassIdComparison comp(SYLLABLE);
     this->FindAllDescendantByComparison(&syllables, &comp);
     for (auto it = syllables.begin(); it != syllables.end(); ++it) {
-        Object *obj = (*it)->FindDescendantByType(SYL);
-        ArrayOfStrAttr attributes;
-        (*it)->GetAttributes(&attributes);
-        bool noFollows = std::find_if(attributes.begin(), attributes.end(), [](auto att) -> bool {
-            return (std::string{ "follows" }.compare(att.first) == 0);
-        }) == attributes.end();
-        if (noFollows && (obj == NULL)) {
-            Syl *syl = new Syl();
-            Text *text = new Text();
-            syl->AddChild(text);
-            (*it)->AddChild(syl);
-        }
+        Syllable *syllable = dynamic_cast<Syllable *>(*it);
+        syllable->MarkupAddSyl();
     }
 
     /************ Resolve @facs ************/

commit d31e9e9eb7b87264c3525098d205f70b76cdacc6

Fix regression caused by 7beaab7a83

src/syllable.cpp
@@ -65,25 +65,20 @@ void Syllable::Reset()
 
 bool Syllable::MarkupAddSyl()
 {
-    // Check for a syl
     Object *obj = this->FindDescendantByType(SYL);
-    if (obj != NULL) {  // A syl exists
-        return false;
-    }
-    // Check for follows attribute (extension of a syllable)
     ArrayOfStrAttr attributes;
     this->GetAttributes(&attributes);
-    bool hasFollows = std::find_if(attributes.begin(), attributes.end(), [](auto att) -> bool {
-        return std::string{ "follows" }.compare(att.first) == 0;
-    }) != attributes.end();
-    if (hasFollows) {
-        return false;
+    bool noFollows = std::find_if(attributes.begin(), attributes.end(), [](auto att) -> bool {
+        return (std::string{ "follows" }.compare(att.first) == 0);
+    }) == attributes.end();
+    if (noFollows && (obj == NULL)) {
+        Syl *syl = new Syl();
+        Text *text = new Text();
+        syl->AddChild(text);
+        this->AddChild(syl);
+        return true;
     }
-    Syl *syl = new Syl();
-    Text *text = new Text();
-    syl->AddChild(text);
-    this->AddChild(text);
-    return true;
+    return false;
 }
 
 } // namespace vrv