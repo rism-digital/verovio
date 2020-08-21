e08291cebbcb80393d8158ea37ba5a950ce7a7c3
Monceber <monceber.am@gmail.com>
2020-08-21 16:28:13 +0300

    Add script for text font generation
    - Added shell script with commands for text font generation

---
+++ fonts/generate_text_font.sh
@@ -0,0 +1,20 @@
+# This script generates XML file with bounding boxes for the plain font
+# It assumes that shell function for saxon is set in .bashrc. E.g.:
+# function saxon {
+# java -jar /application_path/saxon9he.jar "$@"
+# }
+# export -f saxon
+# Alternatively check generate_all.sh on how to achieve this with aliases
+
+if [ -z "$1" ]
+then
+	echo "Text font name need to be provided as argument."
+	exit 1
+fi
+
+# generate SVG and G2N files for the text font
+fontforge generate_plain_font_metadata.py "$1.ttf"
+# generate bounding boxes SVG for the text font
+saxon "$1.svg" extract-glyphs-all-fonts.xsl > "tmp/$1-bounding-boxes.svg"
+# generate XML file with bounding boxes
+phantomjs generate-bbox.js "tmp/$1-bounding-boxes.svg" "../data/text/$1.xml" "$1.g2n"
