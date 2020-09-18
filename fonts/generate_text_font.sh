cd6451b38e722c3fa9cbf426c9eed7f1119f8ca2
Monceber <monceber.am@gmail.com>
2020-09-18 14:12:05 +0300

    Add start/end comments to the script for better visibility

--- fonts/generate_text_font.sh
+++ fonts/generate_text_font.sh
@@ -16,9 +16,13 @@ scriptdir=$(dirname "$0")
 fontfile="${1%.*}"
 fontname=$(basename $fontfile)
 
+echo "Generating metadata for $fontname font"
+
 # generate SVG and G2N files for the text font
 fontforge "$scriptdir"/generate_plain_font_metadata.py "$1"
 # generate bounding boxes SVG for the text font
 saxon "$fontfile".svg "$scriptdir"/extract-glyphs-all-fonts.xsl > "$scriptdir"/tmp/"$fontname"-bounding-boxes.svg
 # generate XML file with bounding boxes
 phantomjs "$scriptdir"/generate-bbox.js "$scriptdir"/tmp/"$fontname"-bounding-boxes.svg "$scriptdir"/../data/text/"$fontname".xml "$fontfile".g2n
+
+echo "Done!"
