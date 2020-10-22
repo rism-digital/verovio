rism-digital/verovio

commit 424d0b858dcf5e810af99076ed0585dab83e2f9a

Noteheads and notehead modifiers (#1630)
* Add notehead selection helper funciton to the Note class

Handles only supported noteheads so far

* Update notehead selection for notes

- add helper function that selects glyphs for note based on attributes
- clean up logic of notehead selection a bit

* Add handling of the head.mod attribute

Only paren is covered atm, since other glyphs are not implemented.
Additionally add handling of the "filled" attribute for the MusicXML

* Extend list of supported notehead glyphs

- Uncomment cases for noteheads that has been added
- Add support for @glyph.name to support bigger variety of glyphs

* Update logic of handling notehead glyph selection

- Add duration to GetNoteheadGlyph() so that glyph can changed based on duration of the note. This way we can make sure that 'x' glyph would be different for quarter and half notes.

* Minor fixes to GetNoteheadGlyph method

* Add usage of percussion glyphs for whole/half noteheads

* Minor fix for GetNoteheadGlyph

- avoid using auto

src/layerelement.cpp
@@ -456,29 +456,33 @@ int LayerElement::GetDrawingRadius(Doc *doc, bool isInLigature)
     if (!this->Is({ CHORD, NOTE, REST })) return 0;
 
     wchar_t code = 0;
-
     int dur = DUR_4;
+    Staff *staff = dynamic_cast<Staff *>(this->GetFirstAncestor(STAFF));
+    assert(staff);
     if (this->Is(NOTE)) {
         Note *note = dynamic_cast<Note *>(this);
         assert(note);
         dur = note->GetDrawingDur();
         if (note->IsMensuralDur() && !isInLigature) {
-            code = note->GetMensuralSmuflNoteHead();
+            return doc->GetGlyphWidth(note->GetMensuralSmuflNoteHead(), 
+                staff->m_drawingStaffSize, this->GetDrawingCueSize()) / 2;
         }
+        code = note->GetNoteheadGlyph(dur);
     }
     else if (this->Is(CHORD)) {
         Chord *chord = dynamic_cast<Chord *>(this);
         assert(chord);
         dur = chord->GetActualDur();
+        if (dur == DUR_1)
+            code = SMUFL_E0A2_noteheadWhole;
+        else if (dur == DUR_2)
+            code = SMUFL_E0A3_noteheadHalf;
+        else
+            code = SMUFL_E0A4_noteheadBlack;
     }
 
-    Staff *staff = dynamic_cast<Staff *>(this->GetFirstAncestor(STAFF));
-    assert(staff);
     // Mensural note shorter than DUR_BR
-    if (code) {
-        return doc->GetGlyphWidth(code, staff->m_drawingStaffSize, this->GetDrawingCueSize()) / 2;
-    }
-    else if ((dur <= DUR_BR) || ((dur == DUR_1) && isInLigature)) {
+    if ((dur <= DUR_BR) || ((dur == DUR_1) && isInLigature)) {
         int widthFactor = (dur == DUR_MX) ? 2 : 1;
         if (staff->m_drawingNotationType == NOTATIONTYPE_mensural_black) {
             return widthFactor * doc->GetDrawingBrevisWidth(staff->m_drawingStaffSize) * 0.7;
@@ -487,15 +491,8 @@ int LayerElement::GetDrawingRadius(Doc *doc, bool isInLigature)
             return widthFactor * doc->GetDrawingBrevisWidth(staff->m_drawingStaffSize);
         }
     }
-    else if (dur == DUR_1) {
-        return doc->GetGlyphWidth(SMUFL_E0A2_noteheadWhole, staff->m_drawingStaffSize, this->GetDrawingCueSize()) / 2;
-    }
-    else if (dur == DUR_2) {
-        return doc->GetGlyphWidth(SMUFL_E0A3_noteheadHalf, staff->m_drawingStaffSize, this->GetDrawingCueSize()) / 2;
-    }
-    else {
-        return doc->GetGlyphWidth(SMUFL_E0A4_noteheadBlack, staff->m_drawingStaffSize, this->GetDrawingCueSize()) / 2;
-    }
+
+    return doc->GetGlyphWidth(code, staff->m_drawingStaffSize, this->GetDrawingCueSize()) / 2;
 }
 
 double LayerElement::GetAlignmentDuration(

include/vrv/note.h
@@ -13,6 +13,7 @@
 //----------------------------------------------------------------------------
 
 #include "accid.h"
+#include "atts_externalsymbols.h"
 #include "atts_mensural.h"
 #include "atts_midi.h"
 #include "atts_shared.h"
@@ -49,6 +50,7 @@ class Note : public LayerElement,
              public AttColor,
              public AttColoration,
              public AttCue,
+             public AttExtSym,
              public AttGraced,
              public AttMidiVelocity,
              public AttNoteAnlMensural,
@@ -174,6 +176,11 @@ public:
      * Return the SMuFL code for a mensural note looking at the staff notation type, the coloration and the duration
      */
     wchar_t GetMensuralSmuflNoteHead();
+
+    /**
+     * Return a SMuFL code for the notehead
+     */
+    wchar_t GetNoteheadGlyph(const int duration) const;
 
     /**
      * Check if a note or its parent chord are visible

src/iomei.cpp
@@ -1785,6 +1785,7 @@ void MEIOutput::WriteNote(pugi::xml_node currentNode, Note *note)
     note->WriteColor(currentNode);
     note->WriteColoration(currentNode);
     note->WriteCue(currentNode);
+    note->WriteExtSym(currentNode);
     note->WriteGraced(currentNode);
     note->WriteMidiVelocity(currentNode);
     note->WriteNoteAnlMensural(currentNode);
@@ -4981,6 +4982,7 @@ bool MEIInput::ReadNote(Object *parent, pugi::xml_node note)
     vrvNote->ReadColor(note);
     vrvNote->ReadColoration(note);
     vrvNote->ReadCue(note);
+    vrvNote->ReadExtSym(note);
     vrvNote->ReadGraced(note);
     vrvNote->ReadMidiVelocity(note);
     vrvNote->ReadNoteAnlMensural(note);

src/iomusxml.cpp
@@ -2542,6 +2542,8 @@ void MusicXmlInput::ReadMusicXmlNote(
             note->SetHeadColor(notehead.node().attribute("color").as_string());
             note->SetHeadShape(ConvertNotehead(notehead.node().text().as_string()));
             if (notehead.node().attribute("parentheses").as_bool()) note->SetHeadMod(NOTEHEADMODIFIER_paren);
+            auto noteHeadFill = notehead.node().attribute("filled");
+            if (noteHeadFill) note->SetHeadFill(noteHeadFill.as_bool() ? FILL_solid : FILL_void);
             if (!std::strncmp(notehead.node().text().as_string(), "none", 4)) note->SetHeadVisible(BOOLEAN_false);
         }
 

src/note.cpp
@@ -52,6 +52,7 @@ Note::Note()
     , AttColor()
     , AttColoration()
     , AttCue()
+    , AttExtSym()
     , AttGraced()
     , AttMidiVelocity()
     , AttNoteAnlMensural()
@@ -67,6 +68,7 @@ Note::Note()
     RegisterAttClass(ATT_COLOR);
     RegisterAttClass(ATT_COLORATION);
     RegisterAttClass(ATT_CUE);
+    RegisterAttClass(ATT_EXTSYM);
     RegisterAttClass(ATT_GRACED);
     RegisterAttClass(ATT_NOTEANLMENSURAL);
     RegisterAttClass(ATT_NOTEHEADS);
@@ -91,6 +93,7 @@ void Note::Reset()
     ResetColor();
     ResetColoration();
     ResetCue();
+    ResetExtSym();
     ResetGraced();
     ResetNoteAnlMensural();
     ResetNoteHeads();
@@ -252,7 +255,7 @@ Point Note::GetStemUpSE(Doc *doc, int staffSize, bool isCueSize)
     Point p(defaultXShift, defaultYShift);
 
     // Here we should get the notehead value
-    wchar_t code = SMUFL_E0A4_noteheadBlack;
+    wchar_t code = GetNoteheadGlyph(GetDrawingDur());
 
     // This is never called for now because mensural notes do not have stem/flag children
     // For changingg this, change Note::CalcStem and Note::PrepareLayerElementParts
@@ -283,7 +286,7 @@ Point Note::GetStemDownNW(Doc *doc, int staffSize, bool isCueSize)
     Point p(0, -defaultYShift);
 
     // Here we should get the notehead value
-    wchar_t code = SMUFL_E0A4_noteheadBlack;
+    wchar_t code = GetNoteheadGlyph(GetDrawingDur());
 
     // This is never called for now because mensural notes do not have stem/flag children
     // See comment above
@@ -381,6 +384,53 @@ wchar_t Note::GetMensuralSmuflNoteHead()
         }
     }
     return code;
+}
+
+wchar_t Note::GetNoteheadGlyph(const int duration) const 
+{
+    static std::map<std::string, wchar_t> additionalNoteheadSymbols
+        = { { "noteheadDiamondBlackWide", SMUFL_E0DC_noteheadDiamondBlackWide },
+            { "noteheadDiamondWhiteWide", SMUFL_E0DE_noteheadDiamondWhiteWide },             
+            { "noteheadNull", SMUFL_E0A5_noteheadNull } };
+
+    if (HasGlyphName()) {
+        const std::string glyph = GetGlyphName();
+        if (additionalNoteheadSymbols.end() == additionalNoteheadSymbols.find(glyph)) {
+            return SMUFL_E0A4_noteheadBlack;
+        }
+        return additionalNoteheadSymbols[glyph];
+    }
+
+    switch (GetHeadShape()) {
+        case HEADSHAPE_quarter: return SMUFL_E0A4_noteheadBlack;
+        case HEADSHAPE_half: return SMUFL_E0A3_noteheadHalf;
+        case HEADSHAPE_whole: return SMUFL_E0A2_noteheadWhole;
+        //case HEADSHAPE_backslash: return SMUFL_noteheadBackslash;
+        //case HEADSHAPE_circle: return SMUFL_E0B3_noteheadCircleX;
+        case HEADSHAPE_plus: return SMUFL_E0AF_noteheadPlusBlack;
+        case HEADSHAPE_diamond: {
+            if (DUR_1 == duration) return SMUFL_E0D9_noteheadDiamondHalf;
+            return GetHeadFill() == FILL_void ? SMUFL_E0DD_noteheadDiamondWhite : SMUFL_E0DB_noteheadDiamondBlack;
+        }
+        //case HEADSHAPE_isotriangle: return SMUFL_E0BC_noteheadTriangleUpHalf;
+        //case HEADSHAPE_oval: return SMUFL_noteheadOval;
+        //case HEADSHAPE_piewedge: return SMUFL_noteheadPieWedge;
+        //case HEADSHAPE_rectangle: return SMUFL_noteheadRectangle;
+        //case HEADSHAPE_rtriangle: return SMUFL_noteheadRTriangle;
+        //case HEADSHAPE_semicircle: return SMUFL_noteheadSemicircle;
+        case HEADSHAPE_slash: return SMUFL_E101_noteheadSlashHorizontalEnds;
+        //case HEADSHAPE_square: return SMUFL_noteheadSquare;
+        case HEADSHAPE_x: {
+            if (DUR_1 == duration) return SMUFL_E0B5_noteheadWholeWithX;
+            if (DUR_2 == duration) return SMUFL_E0B6_noteheadHalfWithX;
+            return SMUFL_E0A9_noteheadXBlack;
+        } 
+        default: break;
+    }
+
+    if (DUR_1 == duration) return SMUFL_E0A2_noteheadWhole;
+    if (DUR_2 == duration) return SMUFL_E0A3_noteheadHalf;
+    return SMUFL_E0A4_noteheadBlack;
 }
 
 bool Note::IsVisible()

src/view_element.cpp
@@ -1356,26 +1356,41 @@ void View::DrawNote(DeviceContext *dc, LayerElement *element, Layer *layer, Staf
         else {
             // Whole notes
             wchar_t fontNo;
-            if (drawingDur == DUR_1) {
-                if (note->GetColored() == BOOLEAN_true) {
-                    fontNo = SMUFL_E0FA_noteheadWholeFilled;
-                }
-                else {
-                    fontNo = SMUFL_E0A2_noteheadWhole;
-                }
+            if (note->GetColored() == BOOLEAN_true) {
+                fontNo = (drawingDur == DUR_1) ? SMUFL_E0FA_noteheadWholeFilled : SMUFL_E0A3_noteheadHalf;
             }
-            // Other values
             else {
-                if ((note->GetColored() == BOOLEAN_true) || drawingDur == DUR_2) {
-                    fontNo = SMUFL_E0A3_noteheadHalf;
-                }
-                else {
-                    fontNo = SMUFL_E0A4_noteheadBlack;
-                }
+                fontNo = note->GetNoteheadGlyph(drawingDur);
             }
 
             dc->StartCustomGraphic("notehead");
+
             DrawSmuflCode(dc, noteX, noteY, fontNo, staff->m_drawingStaffSize, drawingCueSize, true);
+
+            // handle notehead enclosure
+            if (note->HasHeadMod()) {
+                switch (note->GetHeadMod()) {
+                    case NOTEHEADMODIFIER_paren: {
+                        DrawSmuflCode(dc, noteX - note->GetDrawingRadius(m_doc), noteY, 
+                            SMUFL_E26A_accidentalParensLeft, staff->m_drawingStaffSize, drawingCueSize, true);
+                        DrawSmuflCode(dc, noteX + note->GetDrawingRadius(m_doc) * 2, noteY,
+                            SMUFL_E26B_accidentalParensRight, staff->m_drawingStaffSize, drawingCueSize, true);
+                        break;
+                    }
+                    case NOTEHEADMODIFIER_slash:
+                    case NOTEHEADMODIFIER_backslash:
+                    case NOTEHEADMODIFIER_vline: 
+                    case NOTEHEADMODIFIER_hline: {
+                        // TODO: Handle other headmodifiers whenever they become available
+                        //wchar_t glyphCode = note->GetNoteheadModifierGlyph();
+                        //int offset = (m_doc->GetGlyphWidth - note->GetDrawingRadius(m_doc) * 2) / 2;
+                        //DrawSmuflCode(dc, noteX - offset, noteY, glyphCode, staff->m_drawingStaffSize, drawingCueSize, true);
+                        break;
+                    }
+                    default: break;
+                }
+            }
+
             dc->EndCustomGraphic();
         }
     }

commit 3b59f01956754fb06c5fda92833cca12a079fe2b

Attempt to fix the regression #1752 introduced with #1630

src/layerelement.cpp
@@ -465,11 +465,11 @@ int LayerElement::GetDrawingRadius(Doc *doc, bool isInLigature)
         assert(note);
         dur = note->GetDrawingDur();
         if (note->IsMensuralDur() && !isInLigature) {
-            return doc->GetGlyphWidth(
-                       note->GetMensuralSmuflNoteHead(), staff->m_drawingStaffSize, this->GetDrawingCueSize())
-                / 2;
+            code = note->GetMensuralSmuflNoteHead();
         }
-        code = note->GetNoteheadGlyph(dur);
+        else {
+            code = note->GetNoteheadGlyph(dur);
+        }
     }
     else if (this->Is(CHORD)) {
         Chord *chord = vrv_cast<Chord *>(this);