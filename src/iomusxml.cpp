5290050991bb1ee6ec4e012718cde07a86aab590
Zach DeCook <zdecook@ccel.org>
2020-11-04 10:50:03 -0500

    - MusicXML Parsing: Prevent memory leak for empty PgHead/PgFoot

--- src/iomusxml.cpp
+++ src/iomusxml.cpp
@@ -582,10 +582,8 @@ bool MusicXmlInput::ReadMusicXml(pugi::xml_node root)
     // generate page head
     pugi::xpath_node_set credits = root.select_nodes("/score-partwise/credit[@page='1']/credit-words");
     if (!credits.empty()) {
-        bool hasHead = false;
-        bool hasFoot = false;
-        PgHead *head = new PgHead();
-        PgFoot *foot = new PgFoot();
+        PgHead *head = NULL;
+        PgFoot *foot = NULL;
         for (pugi::xpath_node_set::const_iterator it = credits.begin(); it != credits.end(); ++it) {
             pugi::xpath_node words = *it;
             Rend *rend = new Rend();
@@ -603,18 +601,22 @@ bool MusicXmlInput::ReadMusicXml(pugi::xml_node root)
                 rend->AttTypography::StrToFontweight(words.node().attribute("font-weight").as_string()));
             rend->AddChild(text);
             if (words.node().attribute("default-y").as_float() < 2 * bottom) {
+                if (!foot) {
+                    foot = new PgFoot();
+                }
                 foot->AddChild(rend);
-                hasFoot = true;
             }
             else {
+                if (!head) {
+                    head = new PgHead();
+                }
                 head->AddChild(rend);
-                hasHead = true;
             }
         }
-        if (hasHead) {
+        if (head) {
             m_doc->m_mdivScoreDef.AddChild(head);
         }
-        if (hasFoot) {
+        if (foot) {
             m_doc->m_mdivScoreDef.AddChild(foot);
         }
     }
