rism-digital/verovio

commit 16c16635cd6919ade06cf55be592939c0bf2ab9b

Improve rendering of the situations when grace notes/groups collide with beams (#1722)
* Adjust beams to avoid overlapping with other layer elements

- added functor to adjust beam vertical positioning to avoid possible underlying elements (grace notes, beams with grace notes, keySig, etc.)

* Improve beam adjustment with clefs inside beam

- added separate functor to the Clef to calculate proper beam adjustments; drawing Y position of the Clef is the anchor point of the glyph and does not represent top position of the clef, hence we need to take into account a part of the height.

* Adjsut clef/beam collision logic

- updated conditions on which beam and clef are considered overlapping and when adjustments are needed

* Fix for clef intersecting with downward beams

- fixed logic to take into account correct proportions of the clef and to apply correct sign to calculation of the beam overlap when stem direction of the beam is DOWN

* Minor calculation fix

- slightly improved calculation of offset for the layerElements to avoid right-on-top situations

src/layerelement.cpp
@@ -1129,6 +1129,41 @@ int LayerElement::SetAlignmentPitchPos(FunctorParams *functorParams)
     return FUNCTOR_CONTINUE;
 }
 
+int LayerElement::AdjustBeams(FunctorParams *functorParams)
+{
+    AdjustBeamParams *params = vrv_params_cast<AdjustBeamParams *>(functorParams);
+    assert(params);
+
+    // ignore elements that are not in the beam or are direct children of the beam
+    if (!params->m_beam || (Is({ NOTE, CHORD }) && (GetParent() == params->m_beam) && !IsGraceNote()))
+        return FUNCTOR_SIBLINGS;
+    if (Is(GRACEGRP)) return FUNCTOR_CONTINUE;
+
+    Staff *staff = vrv_cast<Staff *>(GetFirstAncestor(STAFF));
+    assert(staff);
+
+    // check if top/bottom of the element overlaps with beam coordinates
+    //const int directionBias = (vrv_cast<Beam *>(params->m_beam)->m_drawingPlace == BEAMPLACE_above) ? 1 : -1;
+    int leftMargin = 0, rightMargin = 0;
+
+    if (params->m_directionBias > 0) {
+        leftMargin = GetDrawingTop(params->m_doc, staff->m_drawingStaffSize, true) - params->m_y1;
+        rightMargin = GetDrawingTop(params->m_doc, staff->m_drawingStaffSize, true) - params->m_y2;
+    }
+    else {
+        leftMargin = GetDrawingBottom(params->m_doc, staff->m_drawingStaffSize, true) - params->m_y1;
+        rightMargin = GetDrawingBottom(params->m_doc, staff->m_drawingStaffSize, true) - params->m_y2;
+    }
+
+    const int overlapMargin = std::max(leftMargin * params->m_directionBias, rightMargin * params->m_directionBias);
+    if (overlapMargin >= params->m_directionBias * params->m_overlapMargin) {
+        const int staffOffset = params->m_doc->GetDrawingUnit(staff->m_drawingStaffSize);
+        params->m_overlapMargin = (((overlapMargin + staffOffset - 1) / staffOffset + 1) * staffOffset) * params->m_directionBias;
+    }
+
+    return FUNCTOR_CONTINUE;
+}
+
 int LayerElement::AdjustLayers(FunctorParams *functorParams)
 {
     AdjustLayersParams *params = vrv_params_cast<AdjustLayersParams *>(functorParams);

include/vrv/beam.h
@@ -152,6 +152,16 @@ public:
     //----------//
 
     /**
+     * See Object::AdjustBeams
+     */
+    virtual int AdjustBeams(FunctorParams *);
+
+    /**
+     * See Object::AdjustBeamsEnd
+     */
+    virtual int AdjustBeamsEnd(FunctorParams *);
+
+    /**
      * See Object::CalcStem
      */
     virtual int CalcStem(FunctorParams *functorParams);
@@ -196,6 +206,7 @@ public:
         m_element = NULL;
         m_closestNote = NULL;
         m_stem = NULL;
+        m_overlapMargin = 0;
     }
     virtual ~BeamElementCoord();
 
@@ -214,6 +225,7 @@ public:
     int m_yBeam; // y value of stem top position
     int m_dur; // drawing duration
     int m_breaksec;
+    int m_overlapMargin;
     bool m_centered; // beam is centered on the line
     bool m_shortened; // stem is shortened because pointing oustide the staff
     char m_partialFlags[MAX_DURATION_PARTIALS];

include/vrv/clef.h
@@ -57,6 +57,15 @@ public:
      */
     static int ClefId(data_CLEFSHAPE shape, char line, data_OCTAVE_DIS octaveDis, data_STAFFREL_basic place);
 
+    //----------//
+    // Functors //
+    //----------//
+
+    /**
+     * See Object::AdjustBeams
+     */
+    virtual int AdjustBeams(FunctorParams *functorParams);
+
 private:
 public:
     //

include/vrv/functorparams.h
@@ -150,6 +150,38 @@ public:
 };
 
 //----------------------------------------------------------------------------
+// AdjustBeamParams
+//----------------------------------------------------------------------------
+
+/**
+ * member 0: the beam that should be adjusted
+ * member 1: y coordinate of the beam left side
+ * member 2: y coordinate of the beam right side
+ * member 3: overlap margin that beam needs to be displaced by
+ * member 4: the Doc
+ **/
+
+class AdjustBeamParams : public FunctorParams {
+public:
+    AdjustBeamParams(Doc *doc)
+    {
+        m_beam = NULL;
+        m_y1 = 0;
+        m_y2 = 0;
+        m_directionBias = 0;
+        m_overlapMargin = 0;
+        m_doc = doc;
+    }
+
+    Object *m_beam;
+    int m_y1;
+    int m_y2;
+    int m_directionBias;
+    int m_overlapMargin;
+    Doc *m_doc;
+};
+
+//----------------------------------------------------------------------------
 // AdjustArticWithSlursParams
 //----------------------------------------------------------------------------
 

include/vrv/layerelement.h
@@ -211,6 +211,11 @@ public:
     //----------//
 
     /**
+     * See Object::AdjustBeams
+     */
+    virtual int AdjustBeams(FunctorParams *);
+
+    /**
      * See Object::ResetHorizontalAlignment
      */
     virtual int ResetHorizontalAlignment(FunctorParams *functorParams);

include/vrv/object.h
@@ -805,6 +805,16 @@ public:
     virtual int CalcArtic(FunctorParams *) { return FUNCTOR_CONTINUE; }
 
     /**
+     * Calculate the vertical position adjustment for the beam if it overlaps with layer elements
+     */
+    virtual int AdjustBeams(FunctorParams *) { return FUNCTOR_CONTINUE; }
+
+    /**
+     * Apply position adjustment that has been calculated previously
+     */
+    virtual int AdjustBeamsEnd(FunctorParams *) { return FUNCTOR_CONTINUE; }
+
+    /**
      * Adjust the postion position of slurs.
      */
     virtual int AdjustSlurs(FunctorParams *) { return FUNCTOR_CONTINUE; }

src/beam.cpp
@@ -1069,6 +1069,8 @@ void BeamElementCoord::SetDrawingStemDir(
     else if ((ledgerLines > 1) && (this->m_dur > DUR_16)) {
         this->m_yBeam += (stemDir == STEMDIRECTION_up) ? 2 * unit : -2 * unit;
     }
+
+    this->m_yBeam += m_overlapMargin;
 }
 
 int BeamElementCoord::CalculateStemLength(Staff *staff, data_STEMDIRECTION stemDir)
@@ -1115,6 +1117,55 @@ int BeamElementCoord::CalculateStemLength(Staff *staff, data_STEMDIRECTION stemD
 //----------------------------------------------------------------------------
 // Functors methods
 //----------------------------------------------------------------------------
+
+int Beam::AdjustBeams(FunctorParams *functorParams)
+{
+    AdjustBeamParams *params = vrv_params_cast<AdjustBeamParams *>(functorParams);
+    assert(params);
+
+    // process highest-level beam
+    if (!params->m_beam) {
+        params->m_beam = this;
+        params->m_y1 = (*m_beamSegment.m_beamElementCoordRefs.begin())->m_yBeam;
+        params->m_y2 = m_beamSegment.m_beamElementCoordRefs.back()->m_yBeam;
+        params->m_directionBias = (m_drawingPlace == BEAMPLACE_above) ? 1 : -1;
+        return FUNCTOR_CONTINUE;
+    }
+
+    //const int directionBias = (vrv_cast<Beam *>(params->m_beam)->m_drawingPlace == BEAMPLACE_above) ? 1 : -1;
+
+    const int leftMargin = (*m_beamSegment.m_beamElementCoordRefs.begin())->m_yBeam - params->m_y1;
+    const int rightMargin = m_beamSegment.m_beamElementCoordRefs.back()->m_yBeam - params->m_y2;
+
+    const int overlapMargin = std::max(leftMargin * params->m_directionBias, rightMargin * params->m_directionBias);
+    if (overlapMargin >= params->m_overlapMargin) {
+        Staff *staff = vrv_cast<Staff *>(GetFirstAncestor(STAFF));
+        assert(staff);
+        const int staffOffset = params->m_doc->GetDrawingUnit(staff->m_drawingStaffSize);
+        params->m_overlapMargin = (overlapMargin + staffOffset) * params->m_directionBias;
+    }
+    return FUNCTOR_SIBLINGS;
+}
+
+
+int Beam::AdjustBeamsEnd(FunctorParams *functorParams)
+{
+    AdjustBeamParams *params = vrv_params_cast<AdjustBeamParams *>(functorParams);
+    assert(params);
+
+    if (params->m_beam != this) return FUNCTOR_CONTINUE;
+
+    // set overlap margin for each coord in the beam
+    if (params->m_overlapMargin) {
+        std::for_each(m_beamSegment.m_beamElementCoordRefs.begin(), m_beamSegment.m_beamElementCoordRefs.end(),
+            [overlap = params->m_overlapMargin](BeamElementCoord *coord) { coord->m_overlapMargin = overlap; });
+    }
+    params->m_beam = NULL;
+    params->m_overlapMargin = 0;
+
+    return FUNCTOR_CONTINUE;
+}
+
 
 int Beam::CalcStem(FunctorParams *functorParams)
 {

src/clef.cpp
@@ -14,7 +14,9 @@
 //----------------------------------------------------------------------------
 
 #include "doc.h"
+#include "functorparams.h"
 #include "scoredefinterface.h"
+#include "staff.h"
 
 namespace vrv {
 
@@ -73,6 +75,61 @@ int Clef::GetClefLocOffset() const
 int Clef::ClefId(data_CLEFSHAPE shape, char line, data_OCTAVE_DIS octaveDis, data_STAFFREL_basic place)
 {
     return place << 24 | octaveDis << 16 | line << 8 | shape;
+}
+
+//----------------------------------------------------------------------------
+// Clef functors methods
+//----------------------------------------------------------------------------
+
+int Clef::AdjustBeams(FunctorParams* functorParams)
+{
+    const std::map<data_CLEFSHAPE, std::pair<wchar_t, double> > topToMiddleProportions
+        = { { CLEFSHAPE_G, { SMUFL_E050_gClef, 0.6 } }, 
+            { CLEFSHAPE_C, { SMUFL_E05C_cClef, 0.5 } },
+            { CLEFSHAPE_F, { SMUFL_E062_fClef, 0.35 } }
+    };
+
+    AdjustBeamParams *params = vrv_params_cast<AdjustBeamParams *>(functorParams);
+    assert(params);
+    if (!params->m_beam) return FUNCTOR_SIBLINGS;
+
+    Staff *staff = vrv_cast<Staff *>(GetFirstAncestor(STAFF));
+    assert(staff);
+
+    auto currentShapeIter = topToMiddleProportions.find(GetShape());
+    if (currentShapeIter == topToMiddleProportions.end()) return FUNCTOR_CONTINUE;
+
+    //const int directionBias = (vrv_cast<Beam *>(params->m_beam)->m_drawingPlace == BEAMPLACE_above) ? 1 : -1;
+    const int proportion = (params->m_directionBias > 0) ? 0 : -1;
+
+    // Y position differes for clef shapes, so we need to take into account only a part of the glyph height
+    // Proportion of the glyph about Y point is defined in the topToMiddleProportions map and used when
+    // left and right margins are calculated
+    const int clefPosition = staff->GetDrawingY()
+        - params->m_doc->GetDrawingDoubleUnit(staff->m_drawingStaffSize) * (staff->m_drawingLines - GetLine());
+    const int clefGlyphHeight
+        = params->m_doc->GetGlyphHeight(currentShapeIter->second.first, staff->m_drawingStaffSize, true);
+    const int beamWidth = params->m_doc->GetDrawingBeamWidth(staff->m_drawingStaffSize, false);
+    const int leftMargin = (clefPosition + clefGlyphHeight * (proportion + currentShapeIter->second.second)
+                               + (params->m_directionBias * beamWidth) - params->m_y1)
+        * params->m_directionBias;
+    const int rightMargin = (clefPosition + clefGlyphHeight * (proportion + currentShapeIter->second.second)
+                                + (params->m_directionBias * beamWidth) - params->m_y2)
+        * params->m_directionBias;
+    
+    // If both sides of beam overlap with Clef, we need to get smaller margin, i.e. the one that would make one side not
+    // overlap anymore. For sloped beams this would generally mean that slope will avoid collision as well, for non-sloped
+    // ones it doesn't matter since both ends are at the same Y position
+    const bool bothSidesOverlap = ((leftMargin > params->m_overlapMargin) && (rightMargin > params->m_overlapMargin));
+    const int overlapMargin = bothSidesOverlap ? std::min(leftMargin, rightMargin) : std::max(leftMargin, rightMargin);
+    if ((overlapMargin >= params->m_overlapMargin)
+        && ((overlapMargin >= beamWidth / 2) || (leftMargin == rightMargin))) {
+        const int staffOffset = params->m_doc->GetDrawingUnit(staff->m_drawingStaffSize);
+        params->m_overlapMargin = (overlapMargin / staffOffset + (leftMargin == rightMargin ? 1 : 2)) * staffOffset
+            * params->m_directionBias;
+    }
+
+    return FUNCTOR_CONTINUE;
 }
 
 } // namespace vrv

commit 913780d9d84a3f057cd46251a58c7c059b860688

Fix regression for beam placement
- fixed issue introduced with 16c16635c - children of the beam should not check for overlap with their own parent bem. Changed to loop for the ancestor instead of just checking parent, as these elements can be several layers deep within the beam.

src/layerelement.cpp
@@ -1201,9 +1201,9 @@ int LayerElement::AdjustBeams(FunctorParams *functorParams)
     assert(params);
 
     // ignore elements that are not in the beam or are direct children of the beam
-    if (!params->m_beam || (Is({ NOTE, CHORD }) && (GetParent() == params->m_beam) && !IsGraceNote()))
+    if (!params->m_beam || (Is({ NOTE, CHORD }) && (GetFirstAncestor(BEAM) == params->m_beam) && !IsGraceNote()))
         return FUNCTOR_SIBLINGS;
-    if (Is(GRACEGRP)) return FUNCTOR_CONTINUE;
+    if (Is({GRACEGRP, TUPLET})) return FUNCTOR_CONTINUE;
 
     Staff *staff = vrv_cast<Staff *>(GetFirstAncestor(STAFF));
     assert(staff);