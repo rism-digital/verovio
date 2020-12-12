58a9ab10e6bb76f506252c5e2ec05e4d863bf93f
Stefan Münnich <stefan.muennich@unibas.ch>
2020-12-12 13:35:21 +0100

    Update setup.py

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -73,5 +73,5 @@ setup(name='verovio',
       long_description=read('../../README.md'),
       ext_modules=[verovio_module],
       py_modules=["verovio"],
-      data_files=[('data', ['../../data/*'])],
+      data_files=[('data', ['../../data'])],
       )
