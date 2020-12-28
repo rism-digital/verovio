245cc77cf79fce2ef8f61e3b2a14a39cbe025389
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2020-12-28 10:04:31 +0100

    autopep8 format

--- doc/test-suite.py
+++ doc/test-suite.py
@@ -32,32 +32,35 @@ if __name__ == "__main__":
     tk = verovio.toolkit(False)
     # version of the toolkit
     print(tk.getVersion())
-    
+
     # keep all the options to be able to reset them for each test
     defaultOptions = json.loads(tk.getOptions(True))
-    
+
     tk.setResourcePath('../../data')
 
     path1 = args.test_suite_dir
     path2 = args.output_dir
     dir1 = sorted(os.listdir(path1))
     for item1 in dir1:
-        if not(os.path.isdir(os.path.join(path1, item1))): continue
+        if not(os.path.isdir(os.path.join(path1, item1))):
+            continue
 
         # create the output directory if necessary
         if not(os.path.isdir(os.path.join(path2, item1))):
             os.mkdir(os.path.join(path2, item1))
-        
+
         dir2 = sorted(os.listdir(os.path.join(path1, item1)))
         for item2 in dir2:
             # skip directories
-            if not(os.path.isfile(os.path.join(path1, item1, item2))): continue
+            if not(os.path.isfile(os.path.join(path1, item1, item2))):
+                continue
             # skip hidden files
-            if item2.startswith('.'): continue
+            if item2.startswith('.'):
+                continue
 
             # reset the options
             options = {**defaultOptions, **testOptions}
-            
+
             # filenames (input MEI and output SVG)
             meiFile = os.path.join(path1, item1, item2)
             name, ext = os.path.splitext(item2)
@@ -76,4 +79,3 @@ if __name__ == "__main__":
             tk.setOptions(json.dumps(options))
             tk.loadFile(meiFile)
             tk.renderToSVGFile(svgFile, 1)
-                    
