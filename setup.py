540d8f53cfeb7b5503b3056bb9a7320833d9235f
Laurent Pugin <lxpugin@gmail.com>
2021-01-12 12:05:44 +0100

    escape git rev-list for windows

--- setup.py
+++ setup.py
@@ -46,8 +46,8 @@ def get_version():
         print("Running tools/get_version.sh")
         version = subprocess.getoutput("bash -c 'cd tools; ./get_version.sh'")
     if version.endswith(".dev"):
-        init_sha = subprocess.getoutput("git log -n 1 --pretty=format:%H -- .pypi-version")
-        count = subprocess.getoutput("git rev-list --count HEAD ^{}".format(init_sha))
+        init_sha = subprocess.getoutput("git log -n 1 --pretty=format:%H -- bindings/python/.pypi-version")
+        count = subprocess.getoutput("git rev-list --count HEAD \"^{}\"".format(init_sha))
         version += count
     print(version)
     return version
