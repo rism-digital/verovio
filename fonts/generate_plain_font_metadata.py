2883c63af4a4ced14c495014b44cd8622c605fff
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2021-01-29 10:55:11 +0100

    update font extraction for ttc files

--- fonts/generate_plain_font_metadata.py
+++ fonts/generate_plain_font_metadata.py
@@ -4,11 +4,12 @@ import fontforge
 
 fontFileName = os.sys.argv[1]
 path = fontFileName
-try:
-    font = fontforge.open(path)
-    font.generate(os.path.splitext(fontFileName)[0] + ".svg")
-    font.generate(os.path.splitext(fontFileName)[0] + ".otf", "", "glyph-map-file")
+for font in fontforge.fontsInFile(fontFileName):
+    try:
+        f = fontforge.open(u'%s(%s)' % (fontFileName, font))
+        f.generate(font + ".svg")
+        f.generate(font + ".otf", "", "glyph-map-file")
 
-except EnvironmentError:
-    print("Error opening font file %s!" % fontFileName)
-    os.sys.exit(1)
+    except EnvironmentError:
+        print("Error opening font file %s!" % fontFileName)
+        os.sys.exit(1)
--- fonts/generate_text_font.sh
+++ fonts/generate_text_font.sh
@@ -20,9 +20,13 @@ echo "Generating metadata for $fontname font"
 
 # generate SVG and G2N files for the text font
 fontforge "$scriptdir"/generate_plain_font_metadata.py "$1"
-# generate bounding boxes SVG for the text font
-saxon "$fontfile".svg "$scriptdir"/extract-glyphs-all-fonts.xsl > "$scriptdir"/tmp/"$fontname"-bounding-boxes.svg
-# generate XML file with bounding boxes
-phantomjs "$scriptdir"/generate-bbox.js "$scriptdir"/tmp/"$fontname"-bounding-boxes.svg "$scriptdir"/../data/text/"$fontname".xml "$fontfile".g2n
+find . -type f -name "$fontfile*.svg" -not -path "./tmp/*" -print0 | while IFS= read -r -d '' fname;
+do
+	font="${fname%.*}"
+	# generate bounding boxes SVG for the text font
+	saxon "$font".svg "$scriptdir"/extract-glyphs-all-fonts.xsl > "$scriptdir"/tmp/"$font"-bounding-boxes.svg
+	# generate XML file with bounding boxes
+	phantomjs "$scriptdir"/generate-bbox.js "$scriptdir"/tmp/"$font"-bounding-boxes.svg "$scriptdir"/../data/text/"$font".xml "$font".g2n
+done
 
 echo "Done!"
