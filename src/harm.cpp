rism-digital/verovio

commit 3dd860ad5baf6f79a517547093c9ac280ccd047e

Prevent spacing with empty harm. Fixes #1096

src/harm.cpp
@@ -108,6 +108,11 @@ int Harm::AdjustHarmGrpsSpacing(FunctorParams *functorParams)
     AdjustHarmGrpsSpacingParams *params = dynamic_cast<AdjustHarmGrpsSpacingParams *>(functorParams);
     assert(params);
 
+    // If the harm is empty, do not adjust spacing
+    if (!this->HasContentBB()) {
+        return FUNCTOR_CONTINUE;
+    }
+    
     int currentGrpId = this->GetDrawingGrpId();
 
     // No group ID, nothing to do - should probably never happen

commit 69cc9ed538cd379318bbb56aa5d7059ed994ec31

Fix regression introduced in 3dd860ad5baf6f79a517547093c9ac280ccd047e. Fixes #358

src/harm.cpp
@@ -179,11 +179,6 @@ int Harm::AdjustHarmGrpsSpacing(FunctorParams *functorParams)
     AdjustHarmGrpsSpacingParams *params = vrv_params_cast<AdjustHarmGrpsSpacingParams *>(functorParams);
     assert(params);
 
-    // If the harm is empty, do not adjust spacing
-    if (!this->HasContentBB()) {
-        return FUNCTOR_CONTINUE;
-    }
-
     int currentGrpId = this->GetDrawingGrpId();
 
     // No group ID, nothing to do - should probably never happen
@@ -221,10 +216,15 @@ int Harm::AdjustHarmGrpsSpacing(FunctorParams *functorParams)
     }
 
     // Keep the one with the lowest left position (this will also be the widest)
-    for (auto const &positoner : positioners) {
-        if (!harmPositioner || (harmPositioner->GetContentLeft() > positoner->GetContentLeft())) {
-            harmPositioner = positoner;
+    for (auto const &positioner : positioners) {
+        if (!harmPositioner || (harmPositioner->GetContentLeft() > positioner->GetContentLeft())) {
+            harmPositioner = positioner;
         }
+    }
+
+    // If the harm positioner is missing or is empty, do not adjust spacing
+    if (!harmPositioner || !harmPositioner->HasContentBB()) {
+        return FUNCTOR_SIBLINGS;
     }
 
     /************** Calculate the adjustment **************/