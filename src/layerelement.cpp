720169e915b87a76e47a8d42a888c5903e8892b4
Monceber <monceber.am@gmail.com>
2021-02-18 10:18:35 +0200

    Improve rendering of dots/flags to avoid excessive horizontal space
    - improved logic of adjusting X of layer elements - when there are flags/dots elements, they should impact positioning only if they overlap with any other elements; otherwise position should remain unchanged

--- src/layerelement.cpp
+++ src/layerelement.cpp
@@ -1487,9 +1487,7 @@ int LayerElement::AdjustXPos(FunctorParams *functorParams)
 
     // In case of dots/flags we need to hold off of adjusting upcoming min position right away - if it happens that
     // these elements do not overlap with other elements we can draw them as is and save space
-    Staff *staff = vrv_cast<Staff *>(GetFirstAncestor(STAFF));
-    const int layerCount = staff? staff->GetChildCount(LAYER) : 1;
-    if (Is({ DOTS, FLAG }) && (layerCount > 1)) {
+    if (Is({ DOTS, FLAG })) {
         const int additionalOffset = selfRight - params->m_upcomingMinPos;
         if (additionalOffset > params->m_currentAlignment.m_offset) {
             params->m_currentAlignment.m_offset = additionalOffset;
