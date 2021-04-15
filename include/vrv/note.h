c931240063195dcc4376eede52e1ded47d1a7181
Laurent Pugin <lxpugin@gmail.com>
2021-04-15 08:28:08 +0200

    Fix duplicated method declaration

--- include/vrv/note.h
+++ include/vrv/note.h
@@ -207,11 +207,6 @@ public:
     int CorrectDotsPlacement(Staff *staff, int NoteLoc, int dotLoc, bool isDotShifted);
 
     /**
-     * Correct dots placement depending on other dots present in the current alignment
-     */
-    int CorrectDotsPlacement(Staff *staff, int NoteLoc, int dotLoc, bool isDotShifted);
-
-    /**
      * MIDI timing information
      */
     ///@{
