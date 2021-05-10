6ed250bedc34df540766310f6c9b7bacf6b70a25
David Bauer <63608463+DavidBauer1984@users.noreply.github.com>
2021-05-10 12:58:29 +0200

    Engraving defaults (#2171)
    * Json option handling
    
    - two json options for passing content as string or file path
    - improved synchronisation
    
    * Toolkit handles json within json
    
    * Cleanup
    
    - Added missing interface methods
    - Added some comments
    
    * Clang-format
    
    * Simplified json for engravingDefaults
    
    Moreover:
    - Engraving defaults file ignored in toolkit
    - Special treatment of empty file path for json file options

--- src/options.cpp
+++ src/options.cpp
@@ -40,11 +40,10 @@ std::map<int, std::string> Option::s_systemDivider = { { SYSTEMDIVIDER_none, "no
     { SYSTEMDIVIDER_left, "left" }, { SYSTEMDIVIDER_left_right, "left-right" } };
 
 constexpr const char *engravingDefaults
-    = "{'engravingDefaults':{'thinBarlineThickness':0.15,'lyricLineThickness':0.125,"
-      "'slurMidpointThickness':0.3,'staffLineThickness':0.075,'stemThickness':0.1,'tieMidpointThickness':0.25,'"
-      "hairpinThickness':0.1,'octaveLineThickness':0.1,'thickBarlineThickness':0.5,'tupletBracketThickness':0.1,'"
-      "subBracketThickness':0.5,'bracketThickness':0.5,'repeatEndingLineThickness':0.15, 'textEnclosureThickness': "
-      "0.2}}";
+    = "{'thinBarlineThickness':0.15,'lyricLineThickness':0.125,"
+      "'slurMidpointThickness':0.3,'staffLineThickness':0.075,'stemThickness':0.1,'tieMidpointThickness':0.25,"
+      "'hairpinThickness':0.1,'thickBarlineThickness':0.5,'tupletBracketThickness':0.1,'subBracketThickness':0.1,"
+      "'bracketThickness':0.5,'repeatEndingLineThickness':0.075,'textEnclosureThickness':0.1}";
 
 //----------------------------------------------------------------------------
 // Option
@@ -592,30 +591,92 @@ std::string OptionStaffrel::GetDefaultStrValue() const
 // OptionJson
 //----------------------------------------------------------------------------
 
-void OptionJson::Init(const std::string &defaultValue)
+void OptionJson::CopyTo(Option *option)
 {
-    m_defaultValues.parse(defaultValue);
+    OptionJson *child = dynamic_cast<OptionJson *>(option);
+    assert(child);
+    *child = *this;
+}
+
+void OptionJson::Init(JsonSource source, const std::string &defaultValue)
+{
+    m_source = source;
+    ReadJson(m_defaultValues, defaultValue);
     m_isSet = false;
 }
 
-bool OptionJson::SetValue(const std::string &jsonFilePath)
+JsonSource OptionJson::GetSource() const
 {
-    std::ifstream in(jsonFilePath.c_str());
-    if (!in.is_open()) {
-        return false;
+    return m_source;
+}
+
+jsonxx::Object OptionJson::GetValue(bool getDefault) const
+{
+    return getDefault ? m_defaultValues : m_values;
+}
+
+bool OptionJson::SetValue(const std::string &value)
+{
+    bool ok = ReadJson(m_values, value);
+    if (ok) {
+        m_isSet = true;
+    }
+    else {
+        if (m_source == JsonSource::String) {
+            LogError("Input json is not valid or contains errors");
+        }
+        else {
+            // Input is file path
+            if (value.empty()) {
+                ok = true;
+            }
+            else {
+                LogError("Input file '%s' is not valid or contains errors", value.c_str());
+            }
+        }
+    }
+    return ok;
+}
+
+std::string OptionJson::GetStrValue() const
+{
+    return m_values.json();
+}
+
+std::string OptionJson::GetDefaultStrValue() const
+{
+    return m_defaultValues.json();
+}
+
+bool OptionJson::ReadJson(jsonxx::Object &output, const std::string &input) const
+{
+    bool ok = false;
+    jsonxx::Object content;
+    if (m_source == JsonSource::String) {
+        ok = content.parse(input);
+    }
+    else {
+        // Input is file path
+        std::ifstream in(input.c_str());
+        if (!in.is_open()) {
+            return false;
+        }
+        ok = content.parse(in);
+        in.close();
     }
 
-    jsonxx::Object newValues;
-    if (!newValues.parse(in)) {
-        LogError("Input file '%s' is not valid or contains errors", jsonFilePath.c_str());
-        return false;
+    if (ok) {
+        output = content;
     }
+    return ok;
+}
 
-    m_values = newValues;
-    m_isSet = true;
+bool OptionJson::HasValue(const std::vector<std::string> &jsonNodePath) const
+{
+    const JsonPath valPath = StringPath2NodePath(m_values, jsonNodePath);
+    const JsonPath defPath = StringPath2NodePath(m_defaultValues, jsonNodePath);
 
-    in.close();
-    return true;
+    return (valPath.size() == jsonNodePath.size()) || (defPath.size() == jsonNodePath.size());
 }
 
 int OptionJson::GetIntValue(const std::vector<std::string> &jsonNodePath, bool getDefault) const
@@ -984,10 +1045,14 @@ Options::Options()
     m_dynamDist.Init(1.0, 0.5, 16.0);
     this->Register(&m_dynamDist, "dynamDist", &m_generalLayout);
 
-    m_engravingDefaults.SetInfo(
-        "Engraving defaults", "Path to json file describing defaults for engraving SMuFL elements");
-    m_engravingDefaults.Init(engravingDefaults);
+    m_engravingDefaults.SetInfo("Engraving defaults", "Json describing defaults for engraving SMuFL elements");
+    m_engravingDefaults.Init(JsonSource::String, engravingDefaults);
     this->Register(&m_engravingDefaults, "engravingDefaults", &m_generalLayout);
+
+    m_engravingDefaultsFile.SetInfo(
+        "Engraving defaults file", "Path to json file describing defaults for engraving SMuFL elements");
+    m_engravingDefaultsFile.Init(JsonSource::FilePath, "");
+    this->Register(&m_engravingDefaultsFile, "engravingDefaultsFile", &m_generalLayout);
 
     m_font.SetInfo("Font", "Set the music font");
     m_font.Init("Leipzig");
@@ -1483,7 +1548,7 @@ Options::~Options() {}
 
 void Options::Sync()
 {
-    if (!m_engravingDefaults.IsSet()) return;
+    if (!m_engravingDefaults.IsSet() && !m_engravingDefaultsFile.IsSet()) return;
     // override default or passed engravingDefaults with explicitly set values
     std::list<std::pair<std::string, OptionDbl *>> engravingDefaults = {
         { "staffLineThickness", &m_staffLineWidth }, //
@@ -1508,11 +1573,18 @@ void Options::Sync()
         { "textEnclosureThickness", &m_textEnclosureThickness } //
     };
 
-    for (auto &pair : engravingDefaults) {
+    for (const auto &pair : engravingDefaults) {
         if (pair.second->IsSet()) continue;
 
-        const double jsonValue = m_engravingDefaults.GetDoubleValue({ "engravingDefaults", pair.first });
-        pair.second->SetValueDbl(jsonValue * 2);
+        const std::vector<std::string> jsonNodePath = { pair.first };
+        if (m_engravingDefaultsFile.HasValue(jsonNodePath)) {
+            const double jsonValue = m_engravingDefaultsFile.GetDoubleValue(jsonNodePath);
+            pair.second->SetValueDbl(jsonValue * 2.0); // convert from staff spaces to MEI units
+        }
+        else if (m_engravingDefaults.HasValue(jsonNodePath)) {
+            const double jsonValue = m_engravingDefaults.GetDoubleValue(jsonNodePath);
+            pair.second->SetValueDbl(jsonValue * 2.0); // convert from staff spaces to MEI units
+        }
     }
 }
 
