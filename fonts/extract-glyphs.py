4ba86f5b7f7a195b9d45c760556366b8a0ba1f68
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2021-05-18 16:19:51 +0200

    unify name

--- fonts/extract-glyphs.py
+++ fonts/extract-glyphs.py
@@ -82,17 +82,17 @@ if __name__ == '__main__':
     # (2) Create xml file for each glyph
     for glyph in glyphs:
        name = glyph.attrib['glyph-name']
-       glyph_id = name.split('uni')[-1]
-       if glyph_id in supported_glyph_codes:
+       glyph_code = name.split('uni')[-1]
+       if glyph_code in supported_glyph_codes:
           root = ET.Element('symbol')
-          root.set('id', glyph_id)
+          root.set('id', glyph_code)
           root.set('viewBox', f"0 0 {units_per_em} {units_per_em}")
           root.set('overflow', 'inherit')
           path = ET.SubElement(root, 'path')
           path.set('transform', 'scale(1,-1)')
           if 'd' in glyph.attrib:
              path.set('d', glyph.attrib['d'])
-          file_path = f"../data/{font_family}/{glyph_id}-{supported_glyph_codes[glyph_id]}.xml"
+          file_path = f"../data/{font_family}/{glyph_code}-{supported_glyph_codes[glyph_code]}.xml"
           xml = ET.tostring(root, encoding='unicode')
           write_file_content(file_path, xml)
 
@@ -105,11 +105,11 @@ if __name__ == '__main__':
     fontface.set('units-per-em', units_per_em)
     for glyph in glyphs:
        name = glyph.attrib['glyph-name']
-       glyph_id = name.split('uni')[-1]
-       if glyph_id in supported_glyph_codes:
+       glyph_code = name.split('uni')[-1]
+       if glyph_code in supported_glyph_codes:
           path = ET.SubElement(root, 'path')
-          path.set('name', supported_glyph_codes[glyph_id])
-          path.set('id', glyph_id)
+          path.set('name', supported_glyph_codes[glyph_code])
+          path.set('id', glyph_code)
           horiz_adv_x = glyph.attrib['horiz-adv-x'] if 'horiz-adv-x' in glyph.attrib else ''
           path.set('horiz-adv-x', horiz_adv_x)
           path.set('transform', 'scale(1,-1)')
