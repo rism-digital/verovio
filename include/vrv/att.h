63c00f3b9549a94da7c7070ff170cebdea8f5502
David Bauer <davidbauer.frg@gmail.com>
2021-05-27 22:39:48 +0200

    Pass string arg by reference
    in attribute converter functions

--- include/vrv/att.h
+++ include/vrv/att.h
@@ -118,123 +118,123 @@ public:
 
     /** @name Basic converters for reading */
     ///@{
-    double StrToDbl(std::string value) const;
-    int StrToInt(std::string value) const;
-    data_VU StrToVU(std::string value, bool logWarning = true) const;
+    double StrToDbl(const std::string &value) const;
+    int StrToInt(const std::string &value) const;
+    data_VU StrToVU(const std::string &value, bool logWarning = true) const;
     ///@}
 
     /** @name Converters for writing and reading */
     ///@{
     std::string ArticulationListToStr(data_ARTICULATION_List data) const;
-    data_ARTICULATION_List StrToArticulationList(std::string value, bool logWarning = true) const;
+    data_ARTICULATION_List StrToArticulationList(const std::string &value, bool logWarning = true) const;
 
     std::string BeatrptRendToStr(data_BEATRPT_REND data) const;
-    data_BEATRPT_REND StrToBeatrptRend(std::string value, bool logWarning = true) const;
+    data_BEATRPT_REND StrToBeatrptRend(const std::string &value, bool logWarning = true) const;
 
     std::string DurationToStr(data_DURATION data) const;
-    data_DURATION StrToDuration(std::string value, bool logWarning = true) const;
+    data_DURATION StrToDuration(const std::string &value, bool logWarning = true) const;
 
     std::string FontsizenumericToStr(data_FONTSIZENUMERIC data) const;
-    data_FONTSIZENUMERIC StrToFontsizenumeric(std::string value, bool logWarning = true) const;
+    data_FONTSIZENUMERIC StrToFontsizenumeric(const std::string &value, bool logWarning = true) const;
 
     std::string HexnumToStr(data_HEXNUM data) const;
     data_HEXNUM StrToHexnum(std::string value, bool logWarning = true) const;
 
     std::string KeysignatureToStr(data_KEYSIGNATURE data) const;
-    data_KEYSIGNATURE StrToKeysignature(std::string value, bool logWarning = true) const;
+    data_KEYSIGNATURE StrToKeysignature(const std::string &value, bool logWarning = true) const;
 
     std::string MeasurebeatToStr(data_MEASUREBEAT data) const;
     data_MEASUREBEAT StrToMeasurebeat(std::string value, bool logWarning = true) const;
 
     std::string MeasurementabsToStr(data_MEASUREMENTABS data) const { return VUToStr(data); }
-    data_MEASUREMENTABS StrToMeasurementabs(std::string value, bool logWarning = true) const
+    data_MEASUREMENTABS StrToMeasurementabs(const std::string &value, bool logWarning = true) const
     {
         return StrToVU(value, logWarning);
     }
 
     std::string MeasurementrelToStr(data_MEASUREMENTREL data) const { return VUToStr(data); }
-    data_MEASUREMENTREL StrToMeasurementrel(std::string value, bool logWarning = true) const
+    data_MEASUREMENTREL StrToMeasurementrel(const std::string &value, bool logWarning = true) const
     {
         return StrToVU(value, logWarning);
     }
 
     std::string ModusmaiorToStr(data_MODUSMAIOR data) const;
-    data_MODUSMAIOR StrToModusmaior(std::string value, bool logWarning = true) const;
+    data_MODUSMAIOR StrToModusmaior(const std::string &value, bool logWarning = true) const;
 
     std::string ModusminorToStr(data_MODUSMINOR data) const;
-    data_MODUSMINOR StrToModusminor(std::string value, bool logWarning = true) const;
+    data_MODUSMINOR StrToModusminor(const std::string &value, bool logWarning = true) const;
 
     std::string MidibpmToStr(data_MIDIBPM data) const { return IntToStr(data); }
-    data_MIDIBPM StrToMidibpm(std::string value) const { return StrToInt(value); }
+    data_MIDIBPM StrToMidibpm(const std::string &value) const { return StrToInt(value); }
 
     std::string MidichannelToStr(data_MIDICHANNEL data) const { return IntToStr(data); }
-    data_MIDICHANNEL StrToMidichannel(std::string value) const { return StrToInt(value); }
+    data_MIDICHANNEL StrToMidichannel(const std::string &value) const { return StrToInt(value); }
 
     std::string MidimspbToStr(data_MIDIMSPB data) const { return IntToStr(data); }
-    data_MIDIMSPB StrToMidimspb(std::string value) const { return StrToInt(value); }
+    data_MIDIMSPB StrToMidimspb(const std::string &value) const { return StrToInt(value); }
 
     std::string MidivalueToStr(data_MIDIVALUE data) const { return IntToStr(data); }
-    data_MIDIVALUE StrToMidivalue(std::string value) const { return StrToInt(value); }
+    data_MIDIVALUE StrToMidivalue(const std::string &value) const { return StrToInt(value); }
 
     std::string NcnameToStr(data_NCNAME data) const { return StrToStr(data); }
-    data_NCNAME StrToNcname(std::string value) const { return StrToStr(value); }
+    data_NCNAME StrToNcname(const std::string &value) const { return StrToStr(value); }
 
     std::string OctaveToStr(data_OCTAVE data) const { return IntToStr(data); }
-    data_OCTAVE StrToOctave(std::string value) const { return StrToInt(value); }
+    data_OCTAVE StrToOctave(const std::string &value) const { return StrToInt(value); }
 
     std::string OctaveDisToStr(data_OCTAVE_DIS data) const;
-    data_OCTAVE_DIS StrToOctaveDis(std::string value, bool logWarning = true) const;
+    data_OCTAVE_DIS StrToOctaveDis(const std::string &value, bool logWarning = true) const;
 
     std::string OrientationToStr(data_ORIENTATION data) const;
-    data_ORIENTATION StrToOrientation(std::string value, bool logWarning = true) const;
+    data_ORIENTATION StrToOrientation(const std::string &value, bool logWarning = true) const;
 
     std::string PercentToStr(data_PERCENT data) const;
-    data_PERCENT StrToPercent(std::string value, bool logWarning = true) const;
+    data_PERCENT StrToPercent(const std::string &value, bool logWarning = true) const;
 
     std::string PercentLimitedToStr(data_PERCENT_LIMITED_SIGNED data) const;
-    data_PERCENT_LIMITED StrToPercentLimited(std::string value, bool logWarning = true) const;
+    data_PERCENT_LIMITED StrToPercentLimited(const std::string &value, bool logWarning = true) const;
 
     std::string PercentLimitedSignedToStr(data_PERCENT_LIMITED data) const;
-    data_PERCENT_LIMITED_SIGNED StrToPercentLimitedSigned(std::string value, bool logWarning = true) const;
+    data_PERCENT_LIMITED_SIGNED StrToPercentLimitedSigned(const std::string &value, bool logWarning = true) const;
 
     std::string PitchnameToStr(data_PITCHNAME data) const;
-    data_PITCHNAME StrToPitchname(std::string value, bool logWarning = true) const;
+    data_PITCHNAME StrToPitchname(const std::string &value, bool logWarning = true) const;
 
     std::string ProlatioToStr(data_PROLATIO data) const;
-    data_PROLATIO StrToProlatio(std::string value, bool logWarning = true) const;
+    data_PROLATIO StrToProlatio(const std::string &value, bool logWarning = true) const;
 
     std::string TempusToStr(data_TEMPUS data) const;
-    data_TEMPUS StrToTempus(std::string value, bool logWarning = true) const;
+    data_TEMPUS StrToTempus(const std::string &value, bool logWarning = true) const;
 
     std::string TieToStr(data_TIE data) const;
-    data_TIE StrToTie(std::string value, bool logWarning = true) const;
+    data_TIE StrToTie(const std::string &value, bool logWarning = true) const;
 
     std::string XsdAnyURIListToStr(xsdAnyURI_List data) const;
-    xsdAnyURI_List StrToXsdAnyURIList(std::string value) const;
+    xsdAnyURI_List StrToXsdAnyURIList(const std::string &value) const;
 
     std::string XsdPositiveIntegerListToStr(xsdPositiveInteger_List data) const;
-    xsdPositiveInteger_List StrToXsdPositiveIntegerList(std::string value) const;
+    xsdPositiveInteger_List StrToXsdPositiveIntegerList(const std::string &value) const;
     ///@}
 
     /** @name Converters for writing and reading alternate data types not generated by LibMEI */
     ///@{
     std::string FontsizeToStr(data_FONTSIZE data) const;
-    data_FONTSIZE StrToFontsize(std::string value, bool logWarning = true) const;
+    data_FONTSIZE StrToFontsize(const std::string &value, bool logWarning = true) const;
 
     std::string LinewidthToStr(data_LINEWIDTH data) const;
-    data_LINEWIDTH StrToLinewidth(std::string value, bool logWarning = true) const;
+    data_LINEWIDTH StrToLinewidth(const std::string &value, bool logWarning = true) const;
 
     std::string MidivalueNameToStr(data_MIDIVALUE_NAME data) const;
-    data_MIDIVALUE_NAME StrToMidivalueName(std::string value, bool logWarning = true) const;
+    data_MIDIVALUE_NAME StrToMidivalueName(const std::string &value, bool logWarning = true) const;
 
     std::string MidivaluePanToStr(data_MIDIVALUE_PAN data) const;
-    data_MIDIVALUE_PAN StrToMidivaluePan(std::string value, bool logWarning = true) const;
+    data_MIDIVALUE_PAN StrToMidivaluePan(const std::string &value, bool logWarning = true) const;
     ///@}
 
     /** @name Converters for writing and reading alternate data types unsing other alternate data types */
     ///@{
     std::string PlacementToStr(data_PLACEMENT data) const;
-    data_PLACEMENT StrToPlacement(std::string value, bool logWarning = true) const;
+    data_PLACEMENT StrToPlacement(const std::string &value, bool logWarning = true) const;
     ///@}
 };
 
