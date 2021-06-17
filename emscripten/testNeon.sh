5a813d18223cedc60709928e6907cdc2a31bbfdd
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-06-17 11:35:27 -0400

    Added testNeon.sh for faster verovio testing.

---
+++ emscripten/testNeon.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This is a little script that compiles verovio using buildToolkit
+# and moves the output, verovio.js, into Neon/verovio-util. 
+# It then starts up a local instance of Neon using yarn so the 
+# developer can see the changes as soon as possible. 
+# Please change [location_of_your_Neon] with the actual path. 
+# For example: ~/Desktop/DDMAL
+
+
+# Builds verovio for Neon
+./buildToolkit -x "Gootville,Leipzig,Petaluma" -DHPX
+
+cd build
+
+# Moves the needed verovio.js into Neon
+cp verovio.js verovio_new.js
+
+mv verovio_new.js [location_of_your_Neon]/Neon/verovio-util
+
+[location_of_your_Neon]/Neon/verovio-util
+
+rm verovio.js && mv verovio_new.js verovio.js
+
+# Runs Neon
+yarn build && yarn start
