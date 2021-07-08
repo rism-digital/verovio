d242c67f57510e8a7cd1a181b8be5dc60300e979
David Bauer <davidbauer.frg@gmail.com>
2021-07-08 07:22:14 +0200

    Adapt enclose for KeyAccid
    for extended MEI values

--- src/keyaccid.cpp
+++ src/keyaccid.cpp
@@ -78,15 +78,18 @@ std::wstring KeyAccid::GetSymbolStr() const
     std::wstring symbolStr;
 
     if (this->HasEnclose()) {
-        if (this->GetEnclose() == ENCLOSURE_brack) {
-            symbolStr.push_back(SMUFL_E26C_accidentalBracketLeft);
-            symbolStr.push_back(symc);
-            symbolStr.push_back(SMUFL_E26D_accidentalBracketRight);
-        }
-        else {
-            symbolStr.push_back(SMUFL_E26A_accidentalParensLeft);
-            symbolStr.push_back(symc);
-            symbolStr.push_back(SMUFL_E26B_accidentalParensRight);
+        switch (this->GetEnclose()) {
+            case ENCLOSURE_brack:
+                symbolStr.push_back(SMUFL_E26C_accidentalBracketLeft);
+                symbolStr.push_back(symc);
+                symbolStr.push_back(SMUFL_E26D_accidentalBracketRight);
+                break;
+            case ENCLOSURE_paren:
+                symbolStr.push_back(SMUFL_E26A_accidentalParensLeft);
+                symbolStr.push_back(symc);
+                symbolStr.push_back(SMUFL_E26B_accidentalParensRight);
+                break;
+            default: symbolStr.push_back(symc);
         }
     }
     else {
