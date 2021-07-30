02bdfe0092c8e4cc23bcbb73b4bdff9f7db9e245
Laurent Pugin <lxpugin@gmail.com>
2021-07-30 19:29:03 +0200

    Call ScoreDefSetGrpSymDoc in View::SetPage
    * Also make it go trough the entire Doc to reach intermediate ScoreDef

--- src/system.cpp
+++ src/system.cpp
@@ -397,7 +397,7 @@ int System::ScoreDefSetGrpSym(FunctorParams *functorParams)
 
     if (m_drawingScoreDef) m_drawingScoreDef->Process(params->m_functor, functorParams);
 
-    return FUNCTOR_SIBLINGS;
+    return FUNCTOR_CONTINUE;
 }
 
 int System::ResetHorizontalAlignment(FunctorParams *functorParams)
