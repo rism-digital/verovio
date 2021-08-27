81840cf57109feac08cc7020214b1c14a083cc87
Laurent Pugin <lxpugin@gmail.com>
2021-08-27 10:20:07 +0200

    Fix formatting

--- src/object.cpp
+++ src/object.cpp
@@ -618,7 +618,8 @@ void Object::SetParent(Object *parent)
 bool Object::IsSupportedChild(Object *child)
 {
     // This should never happen because the method should be overridden
-    LogDebug("Method for adding %s to %s should be overridden", child->GetClassName().c_str(), this->GetClassName().c_str());
+    LogDebug(
+        "Method for adding %s to %s should be overridden", child->GetClassName().c_str(), this->GetClassName().c_str());
     // assert(false);
     return false;
 }
