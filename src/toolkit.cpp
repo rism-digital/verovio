6408aece2eda800f94554ddf611d9e4db9e29b0a
Werner Goebl <goebl@mdw.ac.at>
2021-12-20 14:57:34 +0100

    Add breaks='encoded' option to tk.redoLayout()

--- src/toolkit.cpp
+++ src/toolkit.cpp
@@ -1187,6 +1187,9 @@ void Toolkit::RedoLayout()
     if (m_options->m_breaks.GetValue() == BREAKS_line) {
         m_doc.CastOffLineDoc();
     }
+    else if (m_options->m_breaks.GetValue() == BREAKS_encoded) {
+        m_doc.CastOffEncodingDoc();
+    }
     else if (m_options->m_breaks.GetValue() == BREAKS_smart) {
         m_doc.CastOffSmartDoc();
     }
