9dca0cb1eea9f5c83fd27a958c1b0ecca1210772
Laurent Pugin <lxpugin@gmail.com>
2022-01-29 13:56:06 +0100

    Use Timemap class in Doc::ExportToTimemap

--- src/doc.cpp
+++ src/doc.cpp
@@ -55,6 +55,7 @@
 #include "syllable.h"
 #include "system.h"
 #include "text.h"
+#include "timemap.h"
 #include "timestamp.h"
 #include "transposition.h"
 #include "verse.h"
@@ -406,63 +407,12 @@ bool Doc::ExportTimemap(std::string &output, bool includeRests, bool includeMeas
         output = "";
         return false;
     }
+    Timemap timemap;
     Functor generateTimemap(&Object::GenerateTimemap);
-    GenerateTimemapParams generateTimemapParams(&generateTimemap);
+    GenerateTimemapParams generateTimemapParams(&timemap, &generateTimemap);
     this->Process(&generateTimemap, &generateTimemapParams);
 
-    double currentTempo = -1000.0;
-    double newTempo;
-
-    jsonxx::Array timemap;
-
-    for (auto &[tstamp, entry] : generateTimemapParams.m_timemap) {
-        jsonxx::Object o;
-        o << "tstamp" << tstamp;
-        o << "qstamp" << entry.qstamp;
-
-        // on / off
-        if (!entry.notesOn.empty()) {
-            jsonxx::Array notesOn;
-            for (auto note : entry.notesOn) notesOn << note;
-            o << "on" << notesOn;
-        }
-        if (!entry.notesOff.empty()) {
-            jsonxx::Array notesOff;
-            for (auto note : entry.notesOff) notesOff << note;
-            o << "off" << notesOff;
-        }
-
-        // restsOn / restsOff
-        if (includeRests) {
-            if (!entry.restsOn.empty()) {
-                jsonxx::Array restsOn;
-                for (auto rest : entry.restsOn) restsOn << rest;
-                o << "restsOn" << restsOn;
-            }
-            if (!entry.restsOff.empty()) {
-                jsonxx::Array restsOff;
-                for (auto rest : entry.restsOff) restsOff << rest;
-                o << "restsOff" << restsOff;
-            }
-        }
-
-        // tempo
-        if (entry.tempo != -1000.0) {
-            newTempo = entry.tempo;
-            if (newTempo != currentTempo) {
-                currentTempo = newTempo;
-                o << "tempo" << std::to_string(currentTempo);
-            }
-        }
-
-        // measureOn
-        if (includeMeasures && !entry.measureOn.empty()) {
-            o << "measureOn" << entry.measureOn;
-        }
-
-        timemap << o;
-    }
-    output = timemap.json();
+    timemap.ToJson(output);
 
     return true;
 }
