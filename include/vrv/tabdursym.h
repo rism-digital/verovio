d36ec4dc4947ef73bbe22d73bc7bc53115485599
Laurent Pugin <lxpugin@gmail.com>
2022-01-31 08:02:00 +0100

    Make TabDurSym inherit from StemmedDrawingInterface

--- include/vrv/tabdursym.h
+++ include/vrv/tabdursym.h
@@ -9,6 +9,7 @@
 #define __VRV_TABDURSYM_H__
 
 #include "atts_shared.h"
+#include "drawinginterface.h"
 #include "layerelement.h"
 
 namespace vrv {
@@ -20,7 +21,7 @@ namespace vrv {
 /**
  * This class models the MEI <tabDurSym> element.
  */
-class TabDurSym : public LayerElement, public AttNNumberLike {
+class TabDurSym : public LayerElement, public StemmedDrawingInterface, public AttNNumberLike {
 public:
     /**
      * @name Constructors, destructors, and other standard methods
@@ -33,6 +34,16 @@ public:
     std::string GetClassName() const override { return "TabDurSym"; }
     ///@}
 
+    /**
+     * @name Getter to interfaces
+     */
+    ///@{
+    StemmedDrawingInterface *GetStemmedDrawingInterface() override
+    {
+        return dynamic_cast<StemmedDrawingInterface *>(this);
+    }
+    ///@}
+
     /** Override the method since alignment is required */
     bool HasToBeAligned() const override { return true; }
 
@@ -41,9 +52,34 @@ public:
      */
     bool IsSupportedChild(Object *object) override;
 
+    /**
+     * Overwritten method for note
+     */
+    void AddChild(Object *object) override;
+
+    /**
+     * Get the stem up / stem down attachment point.
+     * If necessary look at the glyph anchor (if any).
+     */
+    ///@{
+    Point GetStemUpSE(Doc *doc, int staffSize, bool isCueSize) override;
+    Point GetStemDownNW(Doc *doc, int staffSize, bool isCueSize) override;
+    int CalcStemLenInThirdUnits(Staff *staff, data_STEMDIRECTION stemDir) override;
+    ///@}
+
     //----------//
     // Functors //
     //----------//
+
+    /**
+     * See Object::PrepareLayerElementParts
+     */
+    int PrepareLayerElementParts(FunctorParams *functorParams) override;
+
+    /**
+     * See Object::CalcStem
+     */
+    int CalcStem(FunctorParams *functorParams) override;
 
 protected:
     //
--- src/tabdursym.cpp
+++ src/tabdursym.cpp
@@ -13,7 +13,10 @@
 
 //----------------------------------------------------------------------------
 
+#include "elementpart.h"
 #include "functorparams.h"
+#include "layer.h"
+#include "staff.h"
 
 namespace vrv {
 
@@ -23,7 +26,7 @@ namespace vrv {
 
 static const ClassRegistrar<TabDurSym> s_factory("tabDurSym", TABDURSYM);
 
-TabDurSym::TabDurSym() : LayerElement(TABDURSYM, "tabdursym-"), AttNNumberLike()
+TabDurSym::TabDurSym() : LayerElement(TABDURSYM, "tabdursym-"), StemmedDrawingInterface(), AttNNumberLike()
 {
     this->RegisterAttClass(ATT_NNUMBERLIKE);
 
@@ -35,12 +38,48 @@ TabDurSym::~TabDurSym() {}
 void TabDurSym::Reset()
 {
     LayerElement::Reset();
+    StemmedDrawingInterface::Reset();
     this->ResetNNumberLike();
 }
 
 bool TabDurSym::IsSupportedChild(Object *child)
 {
     return false;
+Point TabDurSym::GetStemUpSE(Doc *doc, int staffSize, bool isCueSize)
+{
+    Point p(0, 0);
+    return p;
+}
+
+Point TabDurSym::GetStemDownNW(Doc *doc, int staffSize, bool isCueSize)
+{
+    Point p(0, 0);
+    return p;
+}
+
+int TabDurSym::CalcStemLenInThirdUnits(Staff *staff, data_STEMDIRECTION stemDir)
+{
+    assert(staff);
+
+    if ((stemDir != STEMDIRECTION_down) && (stemDir != STEMDIRECTION_up)) {
+        return 0;
+    }
+
+    int baseStem = STANDARD_STEMLENGTH * 3;
+
+    return baseStem;
+}
+
+//----------------------------------------------------------------------------
+// Functors methods
+//----------------------------------------------------------------------------
+
+int TabDurSym::PrepareLayerElementParts(FunctorParams *functorParams)
+{
+}
+
+int TabDurSym::CalcStem(FunctorParams *functorParams)
+{
 }
 
 } // namespace vrv
