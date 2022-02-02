ba284fe87cf1a4bffd24a3b443d1c6548bd5875b
Laurent Pugin <lxpugin@gmail.com>
2022-02-02 14:37:13 +0100

    Helper for checking if the tab beams have to be put inside or outside the staff
    * Eventually this could come from a staffDef attribute

--- include/vrv/staff.h
+++ include/vrv/staff.h
@@ -112,6 +112,7 @@ public:
     bool IsTabLuteFrench() { return m_drawingNotationType == NOTATIONTYPE_tab_lute_french; }
     bool IsTabLuteGerman() { return m_drawingNotationType == NOTATIONTYPE_tab_lute_german; }
     bool IsTabLuteItalian() { return m_drawingNotationType == NOTATIONTYPE_tab_lute_italian; }
+    bool IsTabWithBeamOutside() { return false; }
     ///@}
 
     /**
