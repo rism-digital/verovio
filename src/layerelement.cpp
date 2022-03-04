b5449ac3027133eb5e9113aafcc07bb7e3bfae98
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2022-03-04 17:33:18 +0100

    add whole Layer to functor

--- src/layerelement.cpp
+++ src/layerelement.cpp
@@ -1995,6 +1995,12 @@ int LayerElement::PrepareDrawingCueSize(FunctorParams *functorParams)
 {
     if (this->IsScoreDefElement()) return FUNCTOR_SIBLINGS;
 
+    Layer *currentLayer = vrv_cast<Layer *>(this->GetFirstAncestor(LAYER));
+    if (currentLayer->GetCue() == BOOLEAN_true) {
+        m_drawingCueSize = true;
+        return FUNCTOR_CONTINUE;
+    }
+
     if (this->IsGraceNote()) {
         m_drawingCueSize = true;
     }
