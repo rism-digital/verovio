4c06f566d7683cf7b1e481d8b894bb514496430f
Laurent Pugin <lxpugin@gmail.com>
2022-03-08 15:01:45 +0100

    Correct checks in std::foreach [skip-ci]
    * Test suite evaluated locally

--- src/beam.cpp
+++ src/beam.cpp
@@ -403,9 +403,10 @@ bool BeamSegment::NeedToResetPosition(Staff *staff, Doc *doc, BeamDrawingInterfa
     else {
         int adjust = 0;
         std::for_each(m_beamElementCoordRefs.begin(), m_beamElementCoordRefs.end(), [&](BeamElementCoord *coord) {
-            if (coord->m_element && !coord->m_element->Is({ NOTE, CHORD })) return;
+            if (!coord->m_element || !coord->m_element->Is({ NOTE, CHORD })) return;
             int elemY = coord->m_element->GetDrawingY();
             const int diff = std::abs(elemY - coord->m_yBeam);
+            assert(coord->m_stem);
             if (coord->m_stem->GetDrawingStemDir() == STEMDIRECTION_down) {
                 if (elemY <= coord->m_yBeam + topOffset) {
                     if (diff > adjust) adjust = diff + topOffset;
