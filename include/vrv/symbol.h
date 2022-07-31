6b969aec58d4db11e4f6155314b9e3c4dc94c39a
Laurent Pugin <lxpugin@gmail.com>
2022-07-31 10:32:06 +0200

    Add Symbol class

---
+++ include/vrv/symbol.h
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        symbol.h
+// Author:      Laurent Pugin
+// Created:     2022
+// Copyright (c) Authors and others. All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __VRV_SYMBOL_H__
+#define __VRV_SYMBOL_H__
+
+#include "atts_shared.h"
+#include "textelement.h"
+
+namespace vrv {
+
+//----------------------------------------------------------------------------
+// Symbol
+//----------------------------------------------------------------------------
+
+/**
+ * This class models the MEI <symbol> element.
+ */
+class Symbol : public Object {
+public:
+    /**
+     * @name Constructors, destructors, reset and class name methods
+     * Reset method reset all attribute classes
+     */
+    ///@{
+    Symbol();
+    virtual ~Symbol();
+    Object *Clone() const override { return new Symbol(*this); }
+    void Reset() override;
+    std::string GetClassName() const override { return "Symbol"; }
+    ///@}
+
+    /**
+     * Only supported elements will be actually added to the child list.
+     */
+    bool IsSupportedChild(Object *object) override;
+
+private:
+    //
+public:
+    //
+private:
+};
+
+} // namespace vrv
+
+#endif
---
+++ src/symbol.cpp
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        symbol.cpp
+// Author:      Laurent Pugin
+// Created:     2022
+// Copyright (c) Authors and others. All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+
+#include "symbol.h"
+
+//----------------------------------------------------------------------------
+
+#include <cassert>
+
+//----------------------------------------------------------------------------
+
+#include "editorial.h"
+#include "vrv.h"
+
+namespace vrv {
+
+//----------------------------------------------------------------------------
+// Symbol
+//----------------------------------------------------------------------------
+
+static const ClassRegistrar<Symbol> s_factory("symbol", SYMBOL);
+
+Symbol::Symbol() : Object(SYMBOL, "symbol-")
+{
+    this->Reset();
+}
+
+Symbol::~Symbol() {}
+
+void Symbol::Reset()
+{
+    Object::Reset();
+}
+
+bool Symbol::IsSupportedChild(Object *child)
+{
+    return false;
+}
+
+} // namespace vrv
