da001b4b46b5d4faadf8af382413e610ce938616
Laurent Pugin <lxpugin@gmail.com>
2022-08-11 16:39:46 +0200

    Add VisibleSymbol comparison class

--- include/vrv/comparison.h
+++ include/vrv/comparison.h
@@ -17,6 +17,7 @@
 #include "object.h"
 #include "staffdef.h"
 #include "staffgrp.h"
+#include "symbol.h"
 #include "timeinterface.h"
 
 namespace vrv {
@@ -534,6 +535,26 @@ public:
 
 protected:
     const Object *m_objectToExclude;
+};
+
+//----------------------------------------------------------------------------
+// VisibleSymbol
+//----------------------------------------------------------------------------
+/**
+ * This class evaluates if the object is a visible Symbol.
+ */
+class VisibleSymbol : public ClassIdComparison {
+
+public:
+    VisibleSymbol() : ClassIdComparison(SYMBOL) {}
+
+    bool operator()(const Object *object) override
+    {
+        if (!MatchesType(object)) return false;
+        const Symbol *symbol = vrv_cast<const Symbol *>(object);
+        assert(symbol);
+        return (symbol->m_visibility == Visible);
+    }
 };
 
 //----------------------------------------------------------------------------
