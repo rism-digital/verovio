fa3d93ddd5c9747b45d616e9e7354a72c493d6cd
Laurent Pugin <lxpugin@gmail.com>
2022-09-08 14:52:10 +0200

    Rename gootville-1.2_metadata to gootville_metadata for consistency

--- fonts/generate_all.sh
+++ fonts/generate_all.sh
@@ -15,7 +15,7 @@ echo "Generating Leipzig files ..."
 $PYTHON extract-bounding-boxes.py Leipzig.svg json/leipzig_metadata.json ../data/Leipzig.xml
 
 echo "Generating Gootville files ..."
-$PYTHON extract-bounding-boxes.py Gootville.svg json/gootville-1.2_metadata.json ../data/Gootville.xml
+$PYTHON extract-bounding-boxes.py Gootville.svg json/gootville_metadata.json ../data/Gootville.xml
 
 echo "Generating Petaluma files ..."
 $PYTHON extract-bounding-boxes.py Petaluma.svg json/petaluma_metadata.json ../data/Petaluma.xml
