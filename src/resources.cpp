a5b9cf6b17c75507c47c9e8428958efce90fd528
Laurent Pugin <lxpugin@gmail.com>
2022-09-16 08:36:26 +0200

    Change case values to char32_t literals

--- src/resources.cpp
+++ src/resources.cpp
@@ -129,11 +129,11 @@ const Glyph *Resources::GetTextGlyph(wchar_t code) const
 wchar_t Resources::GetSmuflGlyphForUnicodeChar(const wchar_t unicodeChar)
 {
     wchar_t smuflChar = unicodeChar;
-    switch ((char32_t)unicodeChar) {
-        case L'\U0001d109': smuflChar = SMUFL_E045_dalSegno; break;
-        case L'\U0001d10a': smuflChar = SMUFL_E046_daCapo; break;
-        case L'\U0001d10b': smuflChar = SMUFL_E047_segno; break;
-        case L'\U0001d10c': smuflChar = SMUFL_E048_coda; break;
+    switch (unicodeChar) {
+        case U'\U0001d109': smuflChar = SMUFL_E045_dalSegno; break;
+        case U'\U0001d10a': smuflChar = SMUFL_E046_daCapo; break;
+        case U'\U0001d10b': smuflChar = SMUFL_E047_segno; break;
+        case U'\U0001d10c': smuflChar = SMUFL_E048_coda; break;
         default: break;
     }
     return smuflChar;
