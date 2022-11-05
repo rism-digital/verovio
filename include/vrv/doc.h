60fa4fc9228f06d47ab30e3143cb0f5cd3c87d63
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2022-11-05 14:21:03 +0100

    fix typos

--- include/vrv/doc.h
+++ include/vrv/doc.h
@@ -484,12 +484,12 @@ public:
     pugi::xml_document m_header;
 
     /**
-     * A copy of the header tree stored as pugi::xml_document
+     * A copy of the front tree stored as pugi::xml_document
      */
     pugi::xml_document m_front;
 
     /**
-     * A copy of the header tree stored as pugi::xml_document
+     * A copy of the back tree stored as pugi::xml_document
      */
     pugi::xml_document m_back;
 
--- src/iomei.cpp
+++ src/iomei.cpp
@@ -3742,7 +3742,7 @@ bool MEIInput::ReadDoc(pugi::xml_node root)
     back = music.child("back");
     if (!back.empty()) {
         m_doc->m_back.reset();
-        // copy the complete front into the master document
+        // copy the complete back into the master document
         m_doc->m_back.append_copy(back);
     }
 
