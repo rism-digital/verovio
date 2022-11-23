c59e246afa6beeef768b1ac32fed5facba9cf49f
Laurent Pugin <lxpugin@gmail.com>
2022-11-23 17:42:31 +0100

    Fix parameter for SetSmuflWithFallback. Fixes #3137

--- src/view_text.cpp
+++ src/view_text.cpp
@@ -395,7 +395,7 @@ void View::DrawRend(DeviceContext *dc, Rend *rend, TextDrawingParams &params)
     if (rend->HasFontfam() && rend->GetFontfam() == "smufl") {
         // Because we do not have the string at this stage we rely only on the selected font
         // This means fallback will not work for missing glyphs within <rend>
-        rendFont.SetSmuflWithFallback(SMUFL_FONT_SELECTED);
+        rendFont.SetSmuflWithFallback(false);
         rendFont.SetFaceName(m_doc->GetOptions()->m_font.GetValue());
         int pointSize = (rendFont.GetPointSize() != 0) ? rendFont.GetPointSize() : params.m_pointSize;
         rendFont.SetPointSize(pointSize * m_doc->GetMusicToLyricFontSizeRatio());
