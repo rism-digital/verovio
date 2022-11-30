c6f52c6acf8c80bf734149b0a46bfea229590341
Laurent Pugin <lxpugin@gmail.com>
2022-11-30 14:35:35 +0100

    Add --incip option

--- src/options.cpp
+++ src/options.cpp
@@ -1004,6 +1004,10 @@ Options::Options()
     m_humType.SetInfo("Humdrum type", "Include type attributes when importing from Humdrum");
     m_humType.Init(false);
     this->Register(&m_humType, "humType", &m_general);
+    
+    m_incip.SetInfo("Incip", "Read <incip> elements as data input");
+    m_incip.Init(false);
+    this->Register(&m_incip, "incip", &m_general);
 
     m_justifyVertically.SetInfo("Justify vertically", "Justify spacing vertically to fill the page");
     m_justifyVertically.Init(false);
