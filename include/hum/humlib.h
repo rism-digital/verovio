c781cd215a7cd45dc47adb73289984c88ad87883
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2023-03-30 17:33:31 +0200

    sort

--- include/hum/humlib.h
+++ include/hum/humlib.h
@@ -98,9 +98,9 @@ using std::vector;
 #include "pugiconfig.hpp"
 #include "pugixml.hpp"
 
-using pugi::xml_node;
 using pugi::xml_attribute;
 using pugi::xml_document;
+using pugi::xml_node;
 using pugi::xpath_node;
 
 namespace hum {
