861b5945a7f0f7a062b10ce6774343df8e3bfc72
David Bauer <davidbauer.frg@gmail.com>
2023-04-14 12:22:13 +0200

    Add ConvertToPageBased implementation

--- src/convertfunctor.cpp
+++ src/convertfunctor.cpp
@@ -9,6 +9,13 @@
 
 //----------------------------------------------------------------------------
 
+#include "ending.h"
+#include "mdiv.h"
+#include "page.h"
+#include "score.h"
+#include "section.h"
+#include "system.h"
+
 //----------------------------------------------------------------------------
 
 namespace vrv {
@@ -25,66 +32,109 @@ ConvertToPageBasedFunctor::ConvertToPageBasedFunctor(Page *page)
 
 FunctorCode ConvertToPageBasedFunctor::VisitEditorialElement(EditorialElement *editorialElement)
 {
+    assert(m_currentSystem);
+    editorialElement->MoveItselfTo(m_currentSystem);
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitEditorialElementEnd(EditorialElement *editorialElement)
 {
+    if (editorialElement->m_visibility == Visible) {
+        editorialElement->ConvertToPageBasedMilestone(editorialElement, m_currentSystem);
+    }
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitEnding(Ending *ending)
 {
+    assert(m_currentSystem);
+    ending->MoveItselfTo(m_currentSystem);
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitEndingEnd(Ending *ending)
 {
+    ending->ConvertToPageBasedMilestone(ending, m_currentSystem);
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitMeasure(Measure *measure)
 {
-    return FUNCTOR_CONTINUE;
+    // Move itself to the pageBasedSystem - do not process children
+    assert(m_currentSystem);
+    measure->MoveItselfTo(m_currentSystem);
+
+    return FUNCTOR_SIBLINGS;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitMdiv(Mdiv *mdiv)
 {
+    mdiv->MoveItselfTo(m_page);
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitMdivEnd(Mdiv *mdiv)
 {
+    if (mdiv->m_visibility == Visible) {
+        mdiv->ConvertToPageBasedMilestone(mdiv, m_page);
+    }
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitScore(Score *score)
 {
+    assert(!m_currentSystem);
+    score->MoveItselfTo(m_page);
+    System *system = new System();
+    m_currentSystem = system;
+    m_page->AddChild(system);
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitScoreEnd(Score *score)
 {
+    score->ConvertToPageBasedMilestone(score, m_page);
+    m_currentSystem = NULL;
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitScoreDef(ScoreDef *scoreDef)
 {
-    return FUNCTOR_CONTINUE;
+    // Move itself to the pageBasedSystem - do not process children
+    assert(m_currentSystem);
+    scoreDef->MoveItselfTo(m_currentSystem);
+
+    return FUNCTOR_SIBLINGS;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitSection(Section *section)
 {
+    assert(m_currentSystem);
+    section->MoveItselfTo(m_currentSystem);
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitSectionEnd(Section *section)
 {
+    section->ConvertToPageBasedMilestone(section, m_currentSystem);
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ConvertToPageBasedFunctor::VisitSystemElement(SystemElement *systemElement)
 {
+    assert(m_currentSystem);
+    systemElement->MoveItselfTo(m_currentSystem);
+
     return FUNCTOR_CONTINUE;
 }
 
