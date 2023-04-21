00495cde9b806c588f8acdf6b08a0f03843f26e4
David Bauer <davidbauer.frg@gmail.com>
2023-04-21 09:19:16 +0200

    Add ApplyPPUFactor implementation

--- src/miscfunctor.cpp
+++ src/miscfunctor.cpp
@@ -9,6 +9,10 @@
 
 //----------------------------------------------------------------------------
 
+#include "page.h"
+#include "staff.h"
+#include "system.h"
+
 //----------------------------------------------------------------------------
 
 namespace vrv {
@@ -24,26 +28,48 @@ ApplyPPUFactorFunctor::ApplyPPUFactorFunctor()
 
 FunctorCode ApplyPPUFactorFunctor::VisitLayerElement(LayerElement *layerElement)
 {
+    if (layerElement->IsScoreDefElement()) return FUNCTOR_SIBLINGS;
+
+    if (layerElement->m_xAbs != VRV_UNSET) layerElement->m_xAbs /= m_page->GetPPUFactor();
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ApplyPPUFactorFunctor::VisitMeasure(Measure *measure)
 {
+    if (measure->m_xAbs != VRV_UNSET) measure->m_xAbs /= m_page->GetPPUFactor();
+    if (measure->m_xAbs2 != VRV_UNSET) measure->m_xAbs2 /= m_page->GetPPUFactor();
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ApplyPPUFactorFunctor::VisitPage(Page *page)
 {
+    m_page = page;
+    m_page->m_pageWidth /= m_page->GetPPUFactor();
+    m_page->m_pageHeight /= m_page->GetPPUFactor();
+    m_page->m_pageMarginBottom /= m_page->GetPPUFactor();
+    m_page->m_pageMarginLeft /= m_page->GetPPUFactor();
+    m_page->m_pageMarginRight /= m_page->GetPPUFactor();
+    m_page->m_pageMarginTop /= m_page->GetPPUFactor();
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ApplyPPUFactorFunctor::VisitStaff(Staff *staff)
 {
+    if (staff->m_yAbs != VRV_UNSET) staff->m_yAbs /= m_page->GetPPUFactor();
+
     return FUNCTOR_CONTINUE;
 }
 
 FunctorCode ApplyPPUFactorFunctor::VisitSystem(System *system)
 {
+    if (system->m_xAbs != VRV_UNSET) system->m_xAbs /= m_page->GetPPUFactor();
+    if (system->m_yAbs != VRV_UNSET) system->m_yAbs /= m_page->GetPPUFactor();
+    system->m_systemLeftMar *= m_page->GetPPUFactor();
+    system->m_systemRightMar *= m_page->GetPPUFactor();
+
     return FUNCTOR_CONTINUE;
 }
 
