8cac93f0cb20e34ff08adfdc73ac03f2d46cc448
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-04-24 13:49:52 -0400

    generate divLine font

--- include/vrv/smufl.h
+++ include/vrv/smufl.h
@@ -392,6 +392,12 @@ enum {
     SMUFL_E888_tuplet8 = 0xE888,
     SMUFL_E889_tuplet9 = 0xE889,
     SMUFL_E88A_tupletColon = 0xE88A,
+    SMUFL_E8F3_chantDivisioMinima = 0xE8F3,
+    SMUFL_E8F4_chantDivisioMaior = 0xE8F4,
+    SMUFL_E8F5_chantDivisioMaxima = 0xE8F5,
+    SMUFL_E8F6_chantDivisioFinalis = 0xE8F6,
+    SMUFL_E8F7_chantVirgula = 0xE8F7,
+    SMUFL_E8F8_chantCaesura = 0xE8F8,
     SMUFL_E901_mensuralGclefPetrucci = 0xE901,
     SMUFL_E902_chantFclef = 0xE902,
     SMUFL_E903_mensuralFclef = 0xE903,
@@ -581,7 +587,7 @@ enum {
 };
 
 /** The number of glyphs for verification **/
-#define SMUFL_COUNT 556
+#define SMUFL_COUNT 562
 
 } // namespace vrv
 
