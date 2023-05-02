d7553fe46b242588f1968c8d228b3bcb59ad8af1
Laurent Pugin <lxpugin@gmail.com>
2023-05-02 10:06:10 +0200

    Remove unimplemented method

--- include/vrv/neume.h
+++ include/vrv/neume.h
@@ -77,7 +77,6 @@ public:
      */
     bool IsSupportedChild(Object *object) override;
 
-    virtual int GetPosition(LayerElement *element);
     virtual int GetLigatureCount(int position);
     bool IsLastInNeume(const LayerElement *element) const;
 
