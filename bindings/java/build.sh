33b8e3c83fa3403b46bb34b722e3ed3ffca42c36
Laurent Pugin <lxpugin@gmail.com>
2023-05-25 12:12:52 +0200

    Add commented example for JAVA_HOME [skip-ci]

--- bindings/java/build.sh
+++ bindings/java/build.sh
@@ -31,6 +31,7 @@ unamestr=$(uname)
 if [[ "$unamestr" == 'Linux' ]]; then
     PATHS="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux " # paths to java libraries
 elif [[ "$unamestr" == 'Darwin' ]]; then
+    #JAVA_HOME="/opt/homebrew/Cellar/openjdk/20.0.1/libexec/openjdk.jdk/Contents/Home"
     JAVA_HOME=$(/usr/libexec/java_home)
     PATHS="-I$JAVA_HOME/include -I$JAVA_HOME/include/darwin"
 fi
