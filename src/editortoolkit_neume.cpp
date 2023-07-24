5456a4cb645ff21460d4042f92195932a6df9d86
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-07-24 16:23:34 -0400

    Clean up

--- src/editortoolkit_neume.cpp
+++ src/editortoolkit_neume.cpp
@@ -2950,7 +2950,6 @@ bool EditorToolkitNeume::Ungroup(std::string groupType, std::vector<std::string>
                 Nc *nc = dynamic_cast<Nc *>(el);
                 assert(nc);
                 if (nc->HasLigated() && nc->GetLigated() == BOOLEAN_true) {
-                    // ligNum++;
                     firstIsLig = true;
                 }
 
@@ -3003,7 +3002,6 @@ bool EditorToolkitNeume::Ungroup(std::string groupType, std::vector<std::string>
             if (groupType == "nc") {
                 Nc *nc = dynamic_cast<Nc *>(el);
                 assert(nc);
-                // if (nc->HasLigated()) continue;
 
                 if (firstIsLig) {
                     // if 1st is ligature, neglect 2nd, go to the next nc
