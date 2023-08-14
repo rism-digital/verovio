f3a565111fde416729014f189a41f471d2d09633
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-08-14 16:56:28 -0400

    Break when last element

--- src/editortoolkit_neume.cpp
+++ src/editortoolkit_neume.cpp
@@ -2810,6 +2810,7 @@ bool EditorToolkitNeume::Ungroup(std::string groupType, std::vector<std::string>
                 fparent->ReorderByXPos();
                 uuidArray << (*it);
                 it = elementIds.erase(it);
+                if (it == elementIds.end()) break;
                 el = m_doc->GetDrawingPage()->FindDescendantByID(*it);
             }
         }
