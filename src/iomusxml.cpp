33ac21fcc1a7e903764e04cbb0bda5cab86d8955
Chanjung Kim <freiyer.paxbun@gmail.com>
2023-10-20 13:49:51 +0900

    Fix MusicXML ties ending before the starting notes

--- src/iomusxml.cpp
+++ src/iomusxml.cpp
@@ -1702,6 +1702,9 @@ bool MusicXmlInput::ReadMusicXmlMeasure(
 
     this->MatchTies(true);
     if (!m_tieStack.empty()) this->MatchTies(false);
+    for (auto openTie : m_tieStack) {
+        openTie.m_note->SetScoreTimeOnset(-1); // make scoreTimeOnset small for next measure
+    }
 
     // clear stop stacks after each measure
     m_hairpinStopStack.clear();
@@ -1758,7 +1761,6 @@ void MusicXmlInput::MatchTies(bool matchLayers)
             m_tieStopStack.erase(jter);
         }
         else {
-            iter->m_note->SetScoreTimeOnset(-1); // make scoreTimeOnset small for next measure
             ++iter;
         }
     }
