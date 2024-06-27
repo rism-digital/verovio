f7b5cf5b2b708cb0ed0dbc9cb12b993e3b1ca3c1
Yinan Zhou <yinan.a.zhou@gmail.com>
2024-06-27 12:23:48 -0400

    Remove empty system after staff merging
    Refs: https://github.com/DDMAL/Neon/issues/1220

--- src/editortoolkit_neume.cpp
+++ src/editortoolkit_neume.cpp
@@ -1711,6 +1711,8 @@ bool EditorToolkitNeume::MatchHeight(std::string elementId)
 bool EditorToolkitNeume::Merge(std::vector<std::string> elementIds)
 {
     if (!m_doc->GetDrawingPage()) return false;
+    Object *page = m_doc->GetDrawingPage();
+
     ListOfObjects staves;
 
     // Get the staves by element ID and fail if a staff does not exist.
@@ -1777,8 +1779,35 @@ bool EditorToolkitNeume::Merge(std::vector<std::string> elementIds)
         Layer *sourceLayer = vrv_cast<Layer *>(sourceStaff->GetFirst(LAYER));
         fillLayer->MoveChildrenFrom(sourceLayer);
         assert(sourceLayer->GetChildCount() == 0);
-        Object *parent = sourceStaff->GetParent();
-        parent->DeleteChild(sourceStaff);
+
+        // Delete empty staff with its system parent
+        // Move SECTION, PB, and SYSTEM_MILESTONE_END if any
+        Object *system = sourceStaff->GetFirstAncestor(SYSTEM);
+        if (system->FindDescendantByType(SECTION)) {
+            Object *section = system->FindDescendantByType(SECTION);
+            Object *nextSystem = page->GetNext(system, SYSTEM);
+            if (nextSystem) {
+                section = system->DetachChild(section->GetIdx());
+                nextSystem->InsertChild(section, 0);
+            }
+        }
+        if (system->FindDescendantByType(PB)) {
+            Object *pb = system->FindDescendantByType(PB);
+            Object *nextSystem = page->GetNext(system, SYSTEM);
+            if (nextSystem) {
+                pb = system->DetachChild(pb->GetIdx());
+                nextSystem->InsertChild(pb, 1);
+            }
+        }
+        if (system->FindDescendantByType(SYSTEM_MILESTONE_END)) {
+            Object *milestoneEnd = system->FindDescendantByType(SYSTEM_MILESTONE_END);
+            Object *previousSystem = page->GetPrevious(system, SYSTEM);
+            if (previousSystem) {
+                milestoneEnd = system->DetachChild(milestoneEnd->GetIdx());
+                previousSystem->InsertChild(milestoneEnd, previousSystem->GetChildCount());
+            }
+        }
+        page->DeleteChild(system);
     }
     // Set the bounding box for the staff to the new bounds
     Zone *staffZone = fillStaff->GetZone();
@@ -1790,13 +1819,11 @@ bool EditorToolkitNeume::Merge(std::vector<std::string> elementIds)
 
     fillLayer->ReorderByXPos();
 
+    if (m_doc->IsTranscription() && m_doc->HasFacsimile()) m_doc->SyncFromFacsimileDoc();
+
     m_editInfo.import("uuid", fillStaff->GetID());
     m_editInfo.import("status", "OK");
     m_editInfo.import("message", "");
-
-    if (m_doc->IsTranscription() && m_doc->HasFacsimile()) m_doc->SyncFromFacsimileDoc();
-
-    // TODO change zones for staff children
 
     return true;
 }
