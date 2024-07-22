cbd19d3fc7e2b119cc31c111bce0901f14840cf0
Laurent Pugin <lxpugin@gmail.com>
2024-07-22 11:40:23 +0200

    Basic Volpiano input with stemless quarter note output

--- src/iovolpiano.cpp
+++ src/iovolpiano.cpp
@@ -18,21 +18,15 @@
 
 #include "clef.h"
 #include "doc.h"
-#include "keysig.h"
 #include "layer.h"
 #include "mdiv.h"
 #include "measure.h"
-#include "nc.h"
-#include "neume.h"
 #include "note.h"
 #include "score.h"
 #include "section.h"
 #include "staff.h"
 #include "staffdef.h"
 #include "staffgrp.h"
-#include "syl.h"
-#include "syllable.h"
-#include "text.h"
 #include "vrv.h"
 
 //----------------------------------------------------------------------------
@@ -65,16 +59,13 @@ bool VolpianoInput::Import(const std::string &volpiano)
     score->AddChild(section);
 
     Staff *staff = new Staff(1);
-    Measure *measure = new Measure(NEUMELINE, 1);
+    Measure *measure = new Measure(MEASURED, 1);
     Layer *layer = new Layer();
     layer->SetN(1);
 
     staff->AddChild(layer);
     measure->AddChild(staff);
     section->AddChild(measure);
-
-    Syllable *syllable = NULL;
-    Neume *neume = NULL;
 
     static std::map<char, std::pair<data_PITCHNAME, int>> notes = { { '9', { PITCHNAME_g, 3 } },
         { 'a', { PITCHNAME_a, 3 } }, { 'b', { PITCHNAME_b, 3 } }, { 'c', { PITCHNAME_c, 4 } },
@@ -88,43 +79,22 @@ bool VolpianoInput::Import(const std::string &volpiano)
         { 'E', 'e' }, { 'F', 'f' }, { 'G', 'g' }, { 'H', 'h' }, { 'J', 'j' }, { 'K', 'k' }, { 'L', 'l' }, { 'M', 'm' },
         { 'N', 'n' }, { 'O', 'o' }, { 'P', 'p' }, { 'Q', 'q' }, { 'R', 'r' }, { 'S', 's' } };
 
-    int dashCount = 0;
-
     for (char ch : volpiano) {
-        if (ch == '-') {
-            dashCount++;
-            if (dashCount > 1) {
-                neume = NULL;
+        if (notes.contains(ch) || liquescents.contains(ch)) {
+            const bool liquescent = liquescents.contains(ch);
+            if (liquescent) {
+                ch = liquescents.at(ch);
             }
-            if (dashCount > 2) {
-                syllable = NULL;
-            }
-        }
-        else if (notes.contains(ch)) {
-            dashCount = 0;
-            if (!syllable) {
-                syllable = new Syllable();
-                layer->AddChild(syllable);
-                // We add an arbitrary text until spacing is fixed
-                Syl *syl = new Syl();
-                Text *text = new Text();
-                text->SetText(UTF8to32("text"));
-                syl->AddChild(text);
-                syllable->AddChild(syl);
-            }
-            if (!neume) {
-                neume = new Neume();
-                syllable->AddChild(neume);
-            }
-            Nc *nc = new Nc();
+            Note *note = new Note();
+            note->SetDur(DURATION_4);
+            note->SetStemLen(0.0);
             auto [pname, oct] = notes.at(ch);
-            nc->SetPname(pname);
-            nc->SetOct(oct);
-            neume->AddChild(nc);
-        }
-        else if (liquescents.contains(ch)) {
-            // Ignored for now
-            auto [pitch, oct] = notes.at(liquescents.at(ch));
+            note->SetPname(pname);
+            note->SetOct(oct);
+            if (liquescent) {
+                note->SetGrace(GRACE_unknown);
+            }
+            layer->AddChild(note);
         }
     }
 
@@ -136,10 +106,9 @@ bool VolpianoInput::Import(const std::string &volpiano)
     // Arbitray clef
     Clef *clef = new Clef();
     clef->IsAttribute(true);
-    clef->SetLine(3);
+    clef->SetLine(2);
     clef->SetShape(CLEFSHAPE_G);
     staffDef->AddChild(clef);
-    staffDef->SetNotationtype(NOTATIONTYPE_neume);
     staffGrp->AddChild(staffDef);
     m_doc->GetFirstScoreDef()->AddChild(staffGrp);
 
