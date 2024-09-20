de74afbb7530e9eecbab865ad692e3633696e892
Laurent Pugin <lxpugin@gmail.com>
2024-09-20 17:18:05 +0200

    Store cumulated values in proportion

--- src/proport.cpp
+++ src/proport.cpp
@@ -32,6 +32,30 @@ void Proport::Reset()
 {
     LayerElement::Reset();
     this->ResetDurationRatio();
+
+    m_cumulatedNum = VRV_UNSET;
+    m_cumulatedNumbase = VRV_UNSET;
+}
+
+int Proport::GetCumulatedNum() const
+{
+    return (m_cumulatedNum != VRV_UNSET) ? m_cumulatedNum : this->GetNum();
+}
+
+int Proport::GetCumulatedNumbase() const
+{
+    return (m_cumulatedNumbase != VRV_UNSET) ? m_cumulatedNumbase : this->GetNumbase();
+}
+
+void Proport::Cumulate(const Proport *proport)
+{
+    // Unset values are not cumulated
+    if (proport->HasNum() && this->HasNum()) {
+        m_cumulatedNum = this->GetNum() * proport->GetCumulatedNum();
+    }
+    if (proport->HasNumbase() && this->HasNumbase()) {
+        m_cumulatedNumbase = this->GetNumbase() * proport->GetCumulatedNumbase();
+    }
 }
 
 FunctorCode Proport::Accept(Functor &functor)
