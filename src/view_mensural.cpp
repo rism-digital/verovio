ba9c76b9d1bec207440616dbd156631b08cfc7a2
Laurent Pugin <lxpugin@gmail.com>
2024-09-24 21:22:06 +0200

    Place stems up for longa including in mensural notation type
    * Fixes #1511

--- src/view_mensural.cpp
+++ src/view_mensural.cpp
@@ -223,9 +223,15 @@ void View::DrawMaximaToBrevis(DeviceContext *dc, int y, LayerElement *element, L
         if (note->GetStemDir() != STEMDIRECTION_NONE) {
             up = (note->GetStemDir() == STEMDIRECTION_up);
         }
+        // For CMN we rely on the drawing stem dir interface pre-calculated in functors
         else if (staff->m_drawingNotationType == NOTATIONTYPE_NONE
             || staff->m_drawingNotationType == NOTATIONTYPE_cmn) {
             up = (note->GetDrawingStemDir() == STEMDIRECTION_up);
+        }
+        // For mensural just calculate it here
+        else {
+            int verticalCenter = staff->GetDrawingY() - m_doc->GetDrawingUnit(staffSize) * (staff->m_drawingLines - 1);
+            up = (note->GetDrawingY() < verticalCenter);
         }
         shape = (up) ? LIGATURE_STEM_RIGHT_UP : LIGATURE_STEM_RIGHT_DOWN;
     }
