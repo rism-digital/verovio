rism-digital/verovio

commit e37284d55a850365c2bc36b2567707b6588bda9a

Additional checks following code analysis - no change expected
* Test suite evaluated locally

src/savefunctor.cpp
@@ -77,7 +77,9 @@ FunctorCode SaveFunctor::VisitFlagEnd(Flag *flag)
 FunctorCode SaveFunctor::VisitMdiv(Mdiv *mdiv)
 {
     MEIOutput *meiOutput = dynamic_cast<MEIOutput *>(m_output);
-    if ((mdiv->m_visibility == Hidden) && meiOutput) {
+    if (!meiOutput) return FUNCTOR_STOP;
+
+    if (mdiv->m_visibility == Hidden) {
         // Do not output hidden mdivs in page-based MEI or when saving score-based MEI with filter
         if (!meiOutput->GetScoreBasedMEI() || meiOutput->HasFilter()) return FUNCTOR_SIBLINGS;
     }
@@ -87,7 +89,9 @@ FunctorCode SaveFunctor::VisitMdiv(Mdiv *mdiv)
 FunctorCode SaveFunctor::VisitMdivEnd(Mdiv *mdiv)
 {
     MEIOutput *meiOutput = dynamic_cast<MEIOutput *>(m_output);
-    if ((mdiv->m_visibility == Hidden) && meiOutput) {
+    if (!meiOutput) return FUNCTOR_STOP;
+
+    if (mdiv->m_visibility == Hidden) {
         // Do not output hidden mdivs in page-based MEI or when saving score-based MEI with filter
         if (!meiOutput->GetScoreBasedMEI() || meiOutput->HasFilter()) return FUNCTOR_SIBLINGS;
     }

src/adjustharmgrpsspacingfunctor.cpp
@@ -121,6 +121,9 @@ FunctorCode AdjustHarmGrpsSpacingFunctor::VisitHarm(Harm *harm)
         xShift = m_previousMeasure->GetWidth();
     }
 
+    assert(m_previousHarmStart);
+    assert(m_previousHarmPositioner);
+
     int overlap = m_previousHarmPositioner->GetContentRight() - (harmPositioner->GetContentLeft() + xShift);
     // Two units as default spacing
     int wordSpace = 2 * m_doc->GetDrawingUnit(100);

src/adjustxposfunctor.cpp
@@ -143,7 +143,7 @@ FunctorCode AdjustXPosFunctor::VisitLayerElement(LayerElement *layerElement)
         m_upcomingMinPos += (-offset);
     }
 
-    int selfRight = layerElement->GetAlignment()->GetXRel();
+    int selfRight;
     if (!layerElement->HasSelfBB() || layerElement->HasEmptyBB()) {
         selfRight = layerElement->GetAlignment()->GetXRel();
         // Still add the right margin for the barlines but not with non measure music
@@ -354,6 +354,7 @@ std::pair<int, int> AdjustXPosFunctor::CalculateXPosOffset(LayerElement *layerEl
         int margin = (m_doc->GetRightMargin(bboxElement) + selfLeftMargin) * drawingUnit;
         if (bboxElement->Is(NOTE)) {
             Note *note = vrv_cast<Note *>(bboxElement);
+            assert(note);
             if (note->HasStemMod() && note->GetStemMod() < STEMMODIFIER_MAX) {
                 const int tremWidth = m_doc->GetGlyphWidth(SMUFL_E220_tremolo1, m_staffSize, false);
                 margin = std::max(margin, drawingUnit / 3 + tremWidth / 2);
@@ -386,6 +387,7 @@ std::pair<int, int> AdjustXPosFunctor::CalculateXPosOffset(LayerElement *layerEl
         }
         else if (layerElement->Is(ACCID) && bboxElement->Is(REST)) {
             Rest *rest = vrv_cast<Rest *>(bboxElement);
+            assert(rest);
             const bool hasExplicitLoc = ((rest->HasOloc() && rest->HasPloc()) || rest->HasLoc());
             if (rest->IsInBeam() && !hasExplicitLoc) {
                 overlap = std::max(overlap, bboxElement->GetSelfRight() - layerElement->GetSelfLeft() + margin);

src/calcstemfunctor.cpp
@@ -654,7 +654,9 @@ void CalcStemFunctor::AdjustFlagPlacement(
         note = vrv_cast<Note *>(parent);
     }
     else if (parent->Is(CHORD)) {
-        note = vrv_cast<Chord *>(parent)->GetTopNote();
+        Chord *chord = vrv_cast<Chord *>(parent);
+        assert(chord);
+        note = chord->GetTopNote();
     }
     int ledgerAbove = 0;
     int ledgerBelow = 0;

src/drawinginterface.cpp
@@ -189,6 +189,7 @@ void BeamDrawingInterface::InitCoords(const ListOfObjects &childList, Staff *sta
         // Check if some beam chord has cross staff content
         else if (current->Is(CHORD)) {
             Chord *chord = vrv_cast<Chord *>(current);
+            assert(chord);
             for (Note *note : { chord->GetTopNote(), chord->GetBottomNote() }) {
                 if (note->m_crossStaff && (note->m_crossStaff != m_beamStaff)) {
                     m_crossStaffContent = note->m_crossStaff;

src/floatingobject.cpp
@@ -417,6 +417,8 @@ void FloatingPositioner::SetDrawingYRel(int drawingYRel, bool force)
 
 bool FloatingPositioner::HasHorizontalOverlapWith(const BoundingBox *bbox, int unit) const
 {
+    assert(bbox);
+
     int bboxExtenderWidth = 0;
     const FloatingPositioner *bboxPositioner = dynamic_cast<const FloatingPositioner *>(bbox);
     if (bboxPositioner) {

src/horizontalaligner.cpp
@@ -619,7 +619,7 @@ bool Alignment::AddLayerElementRef(LayerElement *element)
         else {
             layerRef = vrv_cast<Layer *>(element->GetFirstAncestor(LAYER));
             if (layerRef) staffRef = vrv_cast<Staff *>(layerRef->GetFirstAncestor(STAFF));
-            if (staffRef) {
+            if (staffRef && layerRef) {
                 layerN = layerRef->GetN();
                 staffN = staffRef->GetN();
             }

src/metersiggrp.cpp
@@ -137,8 +137,10 @@ MeterSig *MeterSigGrp::GetSimplifiedMeterSig() const
                     maxUnit = currentUnit;
                 }
             }
-            newMeterSig->SetUnit(maxUnit);
-            newMeterSig->SetCount({ { currentCount }, MeterCountSign::None });
+            if (newMeterSig) {
+                newMeterSig->SetUnit(maxUnit);
+                newMeterSig->SetCount({ { currentCount }, MeterCountSign::None });
+            }
             break;
         }
         default: {

commit 7f5c3bf6e9cfea4d044e201e8e9a9d99179e1df4

Fix output not written when not MEI
* Regression introduced in https://github.com/rism-digital/verovio/commit/e37284d55a850365c2bc36b2567707b6588bda9a
* Test-suite evaluated locally

src/savefunctor.cpp
@@ -77,9 +77,8 @@ FunctorCode SaveFunctor::VisitFlagEnd(Flag *flag)
 FunctorCode SaveFunctor::VisitMdiv(Mdiv *mdiv)
 {
     MEIOutput *meiOutput = dynamic_cast<MEIOutput *>(m_output);
-    if (!meiOutput) return FUNCTOR_STOP;
 
-    if (mdiv->m_visibility == Hidden) {
+    if (meiOutput && (mdiv->m_visibility == Hidden)) {
         // Do not output hidden mdivs in page-based MEI or when saving score-based MEI with filter
         if (!meiOutput->GetScoreBasedMEI() || meiOutput->HasFilter()) return FUNCTOR_SIBLINGS;
     }
@@ -89,9 +88,8 @@ FunctorCode SaveFunctor::VisitMdiv(Mdiv *mdiv)
 FunctorCode SaveFunctor::VisitMdivEnd(Mdiv *mdiv)
 {
     MEIOutput *meiOutput = dynamic_cast<MEIOutput *>(m_output);
-    if (!meiOutput) return FUNCTOR_STOP;
 
-    if (mdiv->m_visibility == Hidden) {
+    if (meiOutput && (mdiv->m_visibility == Hidden)) {
         // Do not output hidden mdivs in page-based MEI or when saving score-based MEI with filter
         if (!meiOutput->GetScoreBasedMEI() || meiOutput->HasFilter()) return FUNCTOR_SIBLINGS;
     }