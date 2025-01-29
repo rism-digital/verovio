3837c66fbe001421fd72bd6e32956e986db6da68
David Lewis <david.lewis@oerc.ox.ac.uk>
2025-01-29 09:34:59 +0000

    Clang-format linting

--- include/vrv/annotscore.h
+++ include/vrv/annotscore.h
@@ -44,7 +44,7 @@ public:
     ///@{
     PlistInterface *GetPlistInterface() override { return vrv_cast<PlistInterface *>(this); }
     const PlistInterface *GetPlistInterface() const override { return vrv_cast<const PlistInterface *>(this); }
-   TimePointInterface *GetTimePointInterface() override { return vrv_cast<TimePointInterface *>(this); }
+    TimePointInterface *GetTimePointInterface() override { return vrv_cast<TimePointInterface *>(this); }
     const TimePointInterface *GetTimePointInterface() const override
     {
         return vrv_cast<const TimePointInterface *>(this);
