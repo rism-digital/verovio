7e75e6bf10070644f2d89b027c5052b4ad49aaa1
Laurent Pugin <lxpugin@gmail.com>
2025-04-11 13:18:14 +0200

    Process CalcLedgerLinesFunctor in Page::LayoutPitchPos

--- src/page.cpp
+++ src/page.cpp
@@ -671,6 +671,9 @@ void Page::LayOutPitchPos()
 
     CalcStemFunctor calcStem(doc);
     this->Process(calcStem);
+
+    CalcLedgerLinesFunctor calcLedgerLines(doc);
+    this->Process(calcLedgerLines);
 }
 
 int Page::GetContentHeight() const
