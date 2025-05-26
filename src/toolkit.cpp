2559a3831acfc8bb1230f80e7ca2a2119ca6cebe
Laurent Pugin <lxpugin@gmail.com>
2025-05-26 10:57:34 +0200

    Remove deprecated method [skip-ci]

--- src/toolkit.cpp
+++ src/toolkit.cpp
@@ -2243,12 +2243,6 @@ void Toolkit::ClearHumdrumBuffer()
 #endif
 }
 
-void Toolkit::SetInputFrom(FileFormat format)
-{
-    LogWarning("This method is deprecated. Use SetInputFormat(std::string) instead.");
-    m_inputFrom = format;
-}
-
 std::string Toolkit::ConvertMEIToHumdrum(const std::string &meiData)
 {
 #ifndef NO_HUMDRUM_SUPPORT
