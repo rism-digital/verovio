bf6da8a15e8ce95541817310deb7ba289cf9cfe0
Yinan Zhou <yinan.a.zhou@gmail.com>
2025-07-08 11:45:12 -0400

    feat: support quilisma and oriscus shape change
    refs: https://github.com/DDMAL/Neon/issues/1338

--- src/editortoolkit_neume.cpp
+++ src/editortoolkit_neume.cpp
@@ -153,6 +153,13 @@ bool EditorToolkitNeume::ParseEditorAction(const std::string &json_editorAction)
         }
         LogWarning("Could not parse the set liquescent action");
     }
+    else if (action == "setAquitanianElement") {
+        std::string elementId, shape;
+        if (this->ParseSetAquitanianElementAction(json.get<jsonxx::Object>("param"), &elementId, &shape)) {
+            return this->SetAquitanianElement(elementId, shape);
+        }
+        LogWarning("Could not parse the set aquitanian element action");
+    }
     else if (action == "remove") {
         std::string elementId;
         if (this->ParseRemoveAction(json.get<jsonxx::Object>("param"), &elementId)) {
@@ -2089,7 +2096,7 @@ bool EditorToolkitNeume::SetLiquescent(std::string elementId, std::string curve)
 
     Nc *nc = vrv_cast<Nc *>(m_doc->GetDrawingPage()->FindDescendantByID(elementId));
     assert(nc);
-    bool hasLiquscent = nc->GetChildCount();
+    bool hasLiquscent = nc->GetChildCount(LIQUESCENT);
 
     if (curve == "a") {
         curvatureDirection_CURVE curve = curvatureDirection_CURVE_a;
@@ -2114,6 +2121,46 @@ bool EditorToolkitNeume::SetLiquescent(std::string elementId, std::string curve)
         if (hasLiquscent) {
             Liquescent *liquescent = vrv_cast<Liquescent *>(nc->FindDescendantByType(LIQUESCENT));
             nc->DeleteChild(liquescent);
+        }
+    }
+
+    m_doc->GetDrawingPage()->LayOutTranscription(true);
+
+    m_editInfo.import("status", "OK");
+    m_editInfo.import("message", "");
+    return true;
+}
+
+bool EditorToolkitNeume::SetAquitanianElement(std::string elementId, std::string shape)
+{
+    if (!m_doc->GetDrawingPage()) {
+        LogError("Could not get the drawing page.");
+        m_editInfo.import("status", "FAILURE");
+        m_editInfo.import("message", "Could not get the drawing page.");
+        return false;
+    }
+
+    Nc *nc = vrv_cast<Nc *>(m_doc->GetDrawingPage()->FindDescendantByID(elementId));
+    assert(nc);
+    bool hasQuilisma = nc->GetChildCount(QUILISMA);
+    bool hasOriscus = nc->GetChildCount(ORISCUS);
+
+    if (shape == "quilisma") {
+        if (!hasQuilisma && !hasOriscus) {
+            Quilisma *quisma = new Quilisma();
+            nc->AddChild(quisma);
+        }
+    }
+    else if (shape == "oriscus") {
+        if (!hasQuilisma && !hasOriscus) {
+            Oriscus *oriscus = new Oriscus();
+            nc->AddChild(oriscus);
+        }
+    }
+    else {
+        // For unset
+        if (hasQuilisma || hasOriscus) {
+            nc->DeleteChild(nc->GetFirst());
         }
     }
 
@@ -4146,6 +4193,22 @@ bool EditorToolkitNeume::ParseSetLiquescentAction(jsonxx::Object param, std::str
         return false;
     }
     *curve = param.get<jsonxx::String>("curve");
+    return true;
+}
+
+bool EditorToolkitNeume::ParseSetAquitanianElementAction(
+    jsonxx::Object param, std::string *elementId, std::string *shape)
+{
+    if (!param.has<jsonxx::String>("elementId")) {
+        LogWarning("Could not parse 'elementId'");
+        return false;
+    }
+    *elementId = param.get<jsonxx::String>("elementId");
+    if (!param.has<jsonxx::String>("shape")) {
+        LogWarning("Could not parse 'shape'");
+        return false;
+    }
+    *shape = param.get<jsonxx::String>("shape");
     return true;
 }
 
