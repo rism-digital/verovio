5c436d122cbbb417c94cf4d758d36eab164e2321
infojunkie <karim.ratib@gmail.com>
2026-08-16 15:35:22 -0700

    Guard against empty glyph code

--- src/view_element.cpp
+++ src/view_element.cpp
@@ -1170,8 +1170,10 @@ void View::DrawMeterSig(DeviceContext *dc, MeterSig *meterSig, Staff *staff, int
 
     if (meterSig->HasSym() || meterSig->HasGlyphNum() || meterSig->HasGlyphName()) {
         const char32_t code = meterSig->GetSymbolGlyph();
-        this->DrawSmuflCode(dc, x, y, code, glyphSize, false);
-        x += m_doc->GetGlyphWidth(code, glyphSize, false);
+        if (code) {
+            this->DrawSmuflCode(dc, x, y, code, glyphSize, false);
+            x += m_doc->GetGlyphWidth(code, glyphSize, false);
+        }
     }
     else if (meterSig->GetForm() == METERFORM_num) {
         x += this->DrawMeterSigFigures(dc, x, y, meterSig, 0, staff);
